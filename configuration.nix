8486643b2d3f7054c24c543cfe5b00fd3e1cd506
kye <kyesarri@gmail.com>
2023-10-02 11:45:47 +1100

    02.10.23 v1.0 gnome keyring changes

--- configuration.nix
+++ configuration.nix
@@ -8,7 +8,7 @@
   nixpkgs.config.allowUnfree = true;
 
   imports = [ ./hardware-configuration.nix ];
-  security.pam.services.sddm.enableGnomeKeyring = true;
+  security.pam.services.gdm.enableGnomeKeyring = true;
   nix.package = pkgs.nixUnstable; # prefer nixunstable over stable
 
   nix.settings =
@@ -26,13 +26,14 @@
 
   hardware =
   {
-  nvidia.modesetting.enable = true;
-  pulseaudio.enable = false;
-    opengl = {
-      enable = true;
-      driSupport = true;
-      driSupport32Bit = true; # required for steam
-    };
+    nvidia.modesetting.enable = true;
+    pulseaudio.enable = false;
+      opengl =
+      {
+        enable = true;
+        driSupport = true;
+        driSupport32Bit = true; # required for steam
+      };
   };
 
   services =
@@ -42,57 +43,15 @@
     fwupd.enable = true;
     dbus.enable = true;
     gnome.gnome-keyring.enable = true;
-    #gnome3.gnome-keyring.enable = true;
-  };
 
-# bye KDE
-#    xserver = {
-#      enable = true;
-#      layout = "au";
-#      xkbVariant = "";
-#      desktopManager.plasma5.enable = true;
-#      displayManager.defaultSession = "plasma";
-#      displayManager.session =
-#      [
-#        {
-#          manage = "desktop";
-#          name = "plasma5+none";
-#          start = ''exec env KDEWM=${pkgs.plasma-workspace}/bin/startplasma-x11''; # writes a startwm.sh i believe
-# leaving this old configuration showing how to use wm / dm configs # start = ''exec env KDEWM=${pkgs."_2bwm"}/bin/2bwm ${pkgs.plasma-workspace}/bin/startplasma-x11'';
-# do note this does not work as plasma11 starts kdewm by itself, need to mask the service " systemctl --user mask plasma-kwin_x11.service " dont think that works either :)
-#        }
-#      ];
-#
-#      windowManager = {
-#        herbstluftwm.enable = false;
-#        awesome.enable = false;
-#        bspwm.enable = false;
-#        exwm.enable = false;
-#        openbox.enable = false;
-#        i3.enable = false;
-#        "2bwm".enable = false;
-#      };
-#
-#      displayManager.lightdm = {
-#        enable = true;
-#        background = ./wallpaper/nix-wallpaper-nineish-dark-gray.png;
-#        greeters.slick = { # lightdm greeter "slick"
-#          enable = true;
-#          theme.name = "Qogir-Dark";
-#          draw-user-backgrounds = true;
-#        };
-#      };
-#
-#    };
-#  };
-
-  services.xserver =
-    {
-      enable = true;
-      displayManager.gdm =
+    xserver =
       {
         enable = true;
-        wayland = true;
+        displayManager.gdm =
+        {
+          enable = true;
+          wayland = true;
+        };
       };
     };
 
