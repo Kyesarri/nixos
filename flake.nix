300018fef6cb4ed6cb1594fcd9af7860e37a1dfe
kye <kyesarri@gmail.com>
2023-10-11 08:52:10 +1100

    11.10.23 v1.0 more wcp config

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
           home-manager.nixosModules.home-manager
           {
             home-manager.extraSpecialArgs = {inherit inputs;}; # Pass flake input to home-manager
-            nixpkgs.overlays = [import ./packages];
+            # nixpkgs.overlays = [import ./packages/overlay.nix];
           }
         ];
       };
