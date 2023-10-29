092021431fb4d835f15a3fba74f5ec1280ea3de1
kye <kyesarri@gmail.com>
2023-10-30 00:16:19 +1100

    29.10.23 updated flake, added lite-xl

--- home/lite-xl/default.nix
+++ home/lite-xl/default.nix
@@ -3,5 +3,5 @@
   ...
 }:
 {
-  home-manager.users.kel.home.file."./.config/lite-xl/plugins/language_nix".source = ./language_nix; # hard to use a .js in nix so symlink works for me
+  home-manager.users.kel.home.file."./.config/lite-xl/plugins/nix.lua".source = .nix.lua; # hard to use a .js in nix so symlink works for me
 }
