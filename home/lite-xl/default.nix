341a6dfefce750108ee02bbab720a9d2f1f599e3
kye <kyesarri@gmail.com>
2023-11-04 15:15:51 +1100

    04.11.23

--- home/lite-xl/default.nix
+++ home/lite-xl/default.nix
@@ -8,5 +8,9 @@
   home-manager.users.kel.home.file."./.config/lite-xl/plugins/nix.lua".source = ./nix.lua; # symlink from this dir to defined dir
   imports = [
     ./theme.nix
+    ./init.lua.nix
   ];
 }
+
+# need to rename some of the subfiles here, need to define naming scheme.
+# dont mind filename.extension.nix but will see
