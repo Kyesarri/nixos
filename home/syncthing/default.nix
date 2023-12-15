9508bcc8e2b6c547cc77217948828222b7fef27f
kye <kyesarri@gmail.com>
2023-12-15 12:22:51 +1100

    15.12.23

--- home/syncthing/default.nix
+++ home/syncthing/default.nix
@@ -6,6 +6,9 @@
   home-manager.users.kel.home.file.".config/hypr/per-app/syncthing.conf" = {
     text = ''
       exec-once = sleep 3 && syncthingtray
+      windowrule = float, title:Syncthing Tray
+      windowrulev2 = size 1000 1000, title:Syncthing Tray
+
     '';
   };
 
@@ -16,8 +19,5 @@
     };
   };
 
-  users.users.kel.packages = with pkgs; [
-    syncthing
-    syncthingtray
-  ];
+  users.users.kel.packages = with pkgs; [syncthing syncthingtray];
 }
