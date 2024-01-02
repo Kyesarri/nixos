9e72c23210bb412980ca7246a19a841666a63f62
kye <kyesarri@gmail.com>
2024-01-03 08:55:19 +1100

    03.01.24

--- hosts/laptop/per-device.nix
+++ hosts/laptop/per-device.nix
@@ -2,7 +2,8 @@
   home-manager.users.${user}.home.file.".config/hypr/per-device.conf" = {
     text = ''
       monitor=,1920x1080@120,auto,1
-      bindl =, switch:off[Lid Switch], exec, swaylock --screenshots --effect-blur 20x1
+      bindl =, switch:Lid Switch, exec, swaylock --screenshots --effect-blur 3x5 --fade-in 0.7 --effect-vignette 0.1:0.8
+      bind = $mainMod, l, exec, swaylock --screenshots --effect-blur 3x5 --fade-in 0.7 --effect-vignette 0.1:0.8
     '';
   };
 }
