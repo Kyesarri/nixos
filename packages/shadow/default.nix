c39e628ee1da08f9c14f0d760dc8678a065cc5e0
kye <kyesarri@gmail.com>
2024-01-04 11:56:19 +1100

    04.01.24

---
+++ packages/shadow/default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  python3,
+  fetchFromGitHub,
+}:
+python3.pkgs.buildPythonApplication rec {
+  pname = "shadow";
+  version = "unstable-2023-06-28";
+  format = "pyproject";
+
+  src = fetchFromGitHub {
+    owner = "danielfvm";
+    repo = "shadow";
+    rev = "bfba0ceaa3e05aeb5a86e9758641fb898033d69e";
+    hash = "sha256-Zz94ufHEnGFbUDkX8PFVO95ZUveWBEr4w2fCimDXivw=";
+  };
+
+  nativeBuildInputs = [
+    python3.pkgs.poetry-core
+  ];
+
+  propagatedBuildInputs = with python3.pkgs; [
+    cairocffi
+    glfw
+    imageio
+    mouse # not included in poetry-core? stumped here :)
+    pillow
+    pyopengl
+    pyqt6
+    screeninfo
+    xcffib
+  ];
+
+  pythonImportsCheck = ["Shadow Engine"];
+
+  meta = with lib; {
+    description = "A live and interactive wallpaper \"engine\" for Linux and Windows. It supports mixing various formats like glsl-shaders, images, videos and GIFs to create an amazing wallpaper. Additionally you can expand it's functionality with scripts";
+    homepage = "https://github.com/danielfvm/shadow";
+    license = licenses.gpl3Only;
+    maintainers = with maintainers; [];
+  };
+}
