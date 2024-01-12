864dcf42e9b8e1ec3bec14d81b6395fe0ed854d5
kye <kyesarri@gmail.com>
2024-01-12 12:43:01 +1100

    12.01.24 testing containers

---
+++ containers/default.nix
@@ -0,0 +1,12 @@
+{config, ...}: {
+  networking.nat = {
+    enable = true;
+    internalInterfaces = ["ve-+"];
+    externalInterface = "ens3";
+    # Lazy IPv6 connectivity for the container
+    enableIPv6 = true;
+  };
+  imports = [
+    ./nextcloud
+  ];
+}
---
+++ containers/nextcloud/default.nix
@@ -0,0 +1,46 @@
+{
+  config,
+  pkgs,
+}: {
+  ## currently testing the nextcloud container demo from
+  ## https://nixos.wiki/wiki/NixOS_Containers
+  ## feels like i can leverage lots of this in /containers/default.nix
+  ## and leave per-container config in each /containers/container/default.nix
+
+  ## Below is nextcloud specific config
+
+  containers.nextcloud = {
+    autoStart = true;
+    privateNetwork = true;
+    hostAddress = "192.168.87.9"; # is this the host os?
+    localAddress = "192.168.87.8"; # or is this?
+    hostAddress6 = "fc00::1";
+    localAddress6 = "fc00::2";
+
+    config = {
+      config,
+      pkgs,
+      ...
+    }: {
+      services.nextcloud = {
+        enable = true;
+        package = pkgs.nextcloud28;
+        hostName = "localhost";
+        config.adminpassFile = "${pkgs.writeText "adminpass" "test123"}"; # DON'T DO THIS IN PRODUCTION - the password file will be world-readable in the Nix Store!
+      };
+
+      system.stateVersion = "23.11";
+
+      networking = {
+        firewall = {
+          enable = true;
+          allowedTCPPorts = [80];
+        };
+        # Use systemd-resolved inside the container
+        # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
+        useHostResolvConf = mkForce false;
+      };
+      services.resolved.enable = true;
+    };
+  };
+}
