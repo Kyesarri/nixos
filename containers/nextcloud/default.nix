c8e332e0be7ab33fc6a47e10aaf86f5a07ad9720
kye <kyesarri@gmail.com>
2024-01-12 13:02:10 +1100

    12.01.24 testing containers

--- containers/nextcloud/default.nix
+++ containers/nextcloud/default.nix
@@ -40,7 +40,7 @@
         };
         # Use systemd-resolved inside the container
         # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
-        useHostResolvConf = mkForce false;
+        useHostResolvConf = lib.mkForce false;
       };
       services.resolved.enable = true;
     };
