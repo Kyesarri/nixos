6a5ffd6bb754cbbd9256b9a9b5d8a0d610046b75
kye <kyesarri@gmail.com>
2024-01-12 13:01:04 +1100

    12.01.24 testing containers

--- hosts/serv/default.nix
+++ hosts/serv/default.nix
@@ -1,5 +1,8 @@
 let
   scheme = "snazzy";
+  home = "../../home/";
+  cont = "../../containers/";
+  hardware = "../../hardware/"; # not working :)
 in
   {
     config,
@@ -9,27 +12,41 @@ in
     outputs,
     nix-colors,
     user,
-    plymouth_theme,
+    plymouth_theme, # eeeeehhh not sure if this is needed :)
     ...
   }: {
     imports = [
       nix-colors.homeManagerModules.default
-      ./per-device.nix
+      ./per-device.nix # per device hypr config
+      ./hardware.nix # device specific hardware config
 
-      ../headless.nix
-      ./hardware.nix
+      ../headless.nix # base packages, not really "headless"
 
-      ../../hardware/audio
-      ../../hardware/battery
-      ../../hardware/wireless
+      ../../containers # testing nixos containers, all container packages imported by /containers/default.nix currently
 
-      ../../home
+      ################## could use per-device to import wanted containers or even import containers in here
+      ################## leaving /containers/default.nix as just the networking config / basic container confg?
+      ################## could look something like the following:
+      # ../../containers
+      # ../../containers/nextcloud
+      # ../../containers/haos
+      ################## config this way would be similar formatting to how i have the rest of my configs
+      ################## might be the best way forward
+      ################## added line to make symmetrical :)
+
+      ../../hardware/audio # probs worthwile for warning sounds or something
+      ../../hardware/battery # this server comes with its own built-in "ups"
+      ../../hardware/wireless # wont be required for long, moving to m.2 ethernet
+
+      ../../home # home-manaager config
+      ## below packages come with prebaked configs, hypr bindings and probably candy
       ../../home/bottom
       ../../home/dunst
       ../../home/firefox
       ../../home/git
       ../../home/hypr
       ../../home/kitty
+      ../../home/lite-xl
       ../../home/ulauncher
       ../../home/virt
       ../../home/waybar
@@ -44,7 +61,7 @@ in
 
     networking = {
       hostName = "nix-serv";
-      firewall.allowedTCPPorts = [22]; # ssh
+      firewall.allowedTCPPorts = [22]; # ssh, possibly open already but leaving in
     };
 
     services = {
