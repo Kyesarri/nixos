9bda464a165921fc43b433bc7cc0f10c122f0144
kye <kyesarri@gmail.com>
2024-01-12 13:08:32 +1100

    12.01.24 testing containers

--- containers/nextcloud/default.nix
+++ containers/nextcloud/default.nix
@@ -14,8 +14,8 @@
   containers.nextcloud = {
     autoStart = true;
     privateNetwork = true;
-    hostAddress = "192.168.87.9"; # is this the host os?
-    localAddress = "192.168.87.8"; # or is this?
+    # hostAddress = "192.168.87.9"; # is this the host os?
+    localAddress = "192.168.87.9"; # or is this?
     hostAddress6 = "fc00::1";
     localAddress6 = "fc00::2";
 
