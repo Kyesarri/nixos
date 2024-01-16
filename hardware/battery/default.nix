59fac91a2c912eb6853a87c0f261acfc70557932
kye <kyesarri@gmail.com>
2024-01-16 11:32:30 +1100

    14.01.24 why do i keep adding more packages again?

--- hardware/battery/default.nix
+++ hardware/battery/default.nix
@@ -4,13 +4,10 @@
   user,
   ...
 }: {
-  # charge limit set to 75% via asusctl, unsure if this works on toshiba
   # poweralertd fires off battery / charge notifications from upower
   home-manager.users.${user}.home.file.".config/hypr/per-app/battery.conf" = {
     text = ''
       exec-once = sleep 8 && poweralertd
-      exec-once = systemctl start asusd
-      exec-once = sleep 3 && asusctl -c 75
     '';
   };
   # battery monitoring
