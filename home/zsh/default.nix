1adb0de4e4ca1c323be275fc28d3b43c83c6aed0
kye <kyesarri@gmail.com>
2024-02-20 17:09:57 +1100

    20.02.24 further szsh theme

--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -3,19 +3,33 @@
   user,
   ...
 }: {
+  imports = [./theme.nix];
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
     syntaxHighlighting.highlighters = ["main" "brackets" "pattern" "cursor" "line"];
     syntaxHighlighting.patterns = {};
     syntaxHighlighting.styles = {"globbing" = "none";};
     promptInit = "info='n host cpu os wm sh n' fet.sh";
     ohMyZsh = {
       enable = true;
+      # theme = "$HOME/nixos/home/zsh/fino-time-mod";
+      # theme = "omzsh-nix";
       theme = "fino-time";
-      # theme = "fino-time-mod";
-      plugins = ["sudo" "terraform" "systemadmin" "vi-mode" "colorize"];
+      plugins = [
+        "sudo"
+        "terraform"
+        "systemadmin"
+        "vi-mode"
+        "colorize"
+        {
+          name = "omzsh-nix";
+          src = "/home${user}/.config/";
+          file = ".omzsh-nix.zsh-theme";
+        }
+      ];
     };
   };
 }
