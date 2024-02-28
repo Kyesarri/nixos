55659ad4f1cfe4f2c907f8ce52a0a49928092528
kye <kyesarri@gmail.com>
2024-02-28 14:43:38 +1100

    28.02.24 prism config

--- home/prism/default.nix
+++ home/prism/default.nix
@@ -7,12 +7,11 @@
   home-manager.users.${spaghetti.user} = {
     prism = {
       enable = true;
-      wallpapers = "./wallpapers"; # Path to the wallpapers directory in your config. (IMAGES ONLY)
-
-      # There are a few different ways of setting the colorscheme.
-
-      # If you pass the name it will use a lutgen builtin scheme
+      # wallpapers = "/home/${spaghetti.user}/nixos/wallpapers"; # Path to the wallpapers directory in your config. (IMAGES ONLY)
+      wallpapers = ./wallpapers; # Path to the wallpapers directory in your config. (IMAGES ONLY)
       colorscheme = "horizon-dark";
+      outPath = ".config/wallpapers";
+      # outPath = ".config/wallpapers";
 
       # If you pass a list of colors, it will build a scheme from them.
       # They are formatted like base16 schemes
