b83d9064470d7581c7c3291acf06aacbea246b71
kye <kyesarri@gmail.com>
2024-02-28 11:07:55 +1100

    28.02.24 subFlakes? cbf to mess with rn

--- flake.nix
+++ flake.nix
@@ -86,7 +86,21 @@
   in {
     nixosConfigurations = {
       "nix-laptop" = nixpkgs.lib.nixosSystem {
-        imports = [./hosts/laptop/flake.nix];
+        inherit system specialArgs;
+        modules = [
+          ./hosts/laptop # 4800hs / 1650 / 16gb TODO download more ram
+          {environment.systemPackages = [alejandra.defaultPackage.x86_64-linux];} # codium plugins
+          home-manager.nixosModules.home-manager
+          auto-cpufreq.nixosModules.default
+          agenix.nixosModules.default
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              extraSpecialArgs = {inherit nix-colors inputs;};
+            };
+          }
+        ];
       };
       "nix-notebook" = nixpkgs.lib.nixosSystem {
         inherit system specialArgs;
