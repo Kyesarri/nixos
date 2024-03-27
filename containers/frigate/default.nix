b05a3711524b86e11aca8e7e8ef38a169a69a392
kye <kyesarri@gmail.com>
2024-03-28 10:45:37 +1100

    28.03.24 frigate undefined changes

--- containers/frigate/default.nix
+++ containers/frigate/default.nix
@@ -91,8 +91,8 @@
               entry = {
                 ffmpeg.inputs = [
                   {
-                    "path" = "rtmp://user:password@192.168.87.22:1935/h264Preview_01_sub";
-                    roles = ["rtmp" "detect" "record"];
+                    "path" = "rtsp://user:password@192.168.87.22:1935/h264Preview_01_sub";
+                    roles = ["detect" "record"];
                   }
                   #{
                   #  "path" = "rtsp://user:password@192.168.87.22:554/h264Preview_01_main";
