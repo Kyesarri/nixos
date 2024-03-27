2b1b90e4c692cfe778e2021ab549ff9e107ab771
kye <kyesarri@gmail.com>
2024-03-27 12:52:38 +1100

    27.03.24 frigate container changes

--- containers/frigate/default.nix
+++ containers/frigate/default.nix
@@ -17,45 +17,71 @@
     }: {
       system.stateVersion = "23.11";
 
-      services.frigate = {
-        enable = true;
-        hostname = "frigate.nix-serv";
+      services = {
+        resolved.enable = true;
 
-        settings = {
-          mqtt.enabled = false;
-          ffmpeg.hwaccel_args = "preset-vaapi";
-
-          record = {
-            enabled = true;
-            retain = {
-              days = 7;
-              mode = "all";
-            };
-
-            cameras."test1" = {
-              ffmpeg.inputs = [
-                {
-                  path = "rtsp://127.0.0.1:8554/test1";
-                  input_args = "preset-rtsp-restream";
-                  roles = ["record"];
-                }
+        go2rtc = {
+          enable = false;
+          settings = {
+            streams = {
+              "test1" = [
+                "rtsp://10.83.16.12/11"
               ];
+            };
+            rtsp.listen = ":8554";
+            webrtc.listen = ":8555";
+          };
+        };
+        frigate = {
+          enable = true;
+          hostname = "frigate.nix-serv";
+
+          settings = {
+            mqtt.enabled = false;
+            ffmpeg.hwaccel_args = "preset-vaapi";
+
+            record = {
+              enabled = true;
+              retain = {
+                days = 7;
+                mode = "all";
+              };
+
+              cameras."test1" = {
+                ffmpeg.inputs = [
+                  {
+                    path = "rtsp://127.0.0.1:8554/test1";
+                    input_args = "preset-rtsp-restream";
+                    roles = ["record"];
+                  }
+                ];
+              };
+              cameras."test2" = {
+                ffmpeg.inputs = [
+                  {
+                    path = "rtsp://localaccount:localaccount@172.20.65.103:554/stream1";
+                    roles = [
+                      "record"
+                      "detect"
+                    ];
+                  }
+                ];
+              };
             };
           };
         };
-      };
 
-      networking = {
-        defaultGateway = "192.168.87.251";
-        firewall = {
-          enable = true;
-          allowedTCPPorts = [80];
+        networking = {
+          defaultGateway = "192.168.87.251";
+          firewall = {
+            enable = true;
+            allowedTCPPorts = [80];
+          };
+          # Use systemd-resolved inside the container
+          # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
+          useHostResolvConf = lib.mkForce false;
         };
-        # Use systemd-resolved inside the container
-        # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
-        useHostResolvConf = lib.mkForce false;
       };
-      services.resolved.enable = true;
     };
   };
 }
