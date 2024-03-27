d1b0334f9868c78c86c3a2702954c9eb5f8fc24c
kye <kyesarri@gmail.com>
2024-03-27 12:54:00 +1100

    27.03.24 frigate container changes

--- containers/frigate/default.nix
+++ containers/frigate/default.nix
@@ -70,17 +70,16 @@
             };
           };
         };
-
-        networking = {
-          defaultGateway = "192.168.87.251";
-          firewall = {
-            enable = true;
-            allowedTCPPorts = [80];
-          };
-          # Use systemd-resolved inside the container
-          # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
-          useHostResolvConf = lib.mkForce false;
+      };
+      networking = {
+        defaultGateway = "192.168.87.251";
+        firewall = {
+          enable = true;
+          allowedTCPPorts = [80];
         };
+        # Use systemd-resolved inside the container
+        # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
+        useHostResolvConf = lib.mkForce false;
       };
     };
   };
