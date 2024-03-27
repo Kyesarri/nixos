11adbfcd51febb6575c53d88b7659132d1c7e697
kye <kyesarri@gmail.com>
2024-03-28 10:00:40 +1100

    28.03.24 frigate undefined changes

--- containers/frigate/default.nix
+++ containers/frigate/default.nix
@@ -4,11 +4,14 @@
   lib,
   ...
 }: {
+  # declare some basicboi host settings for container
   containers.frigate = {
     autoStart = true;
     privateNetwork = true; # seperate from host network interface
     hostBridge = "br0";
     localAddress = "192.168.87.7/24"; # container ip
+
+    # container nix config
 
     config = {
       config,
@@ -16,14 +19,14 @@
       ...
     }: {
       system.stateVersion = "23.11";
+
       networking = {
         defaultGateway = "192.168.87.251";
         firewall = {
           enable = false;
           allowedTCPPorts = [80];
         };
-        # Use systemd-resolved inside the container
-        # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
+        # workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
         useHostResolvConf = lib.mkForce false;
       };
 
@@ -42,10 +45,12 @@
 
         resolved.enable = true;
 
+        # frigate container service
         frigate = {
           enable = true;
           hostname = "frigate.nix-serv";
 
+          # settings written to /var/lib/frigate/frigate.yml its not?
           settings = {
             mqtt.enabled = false;
             ffmpeg.hwaccel_args = "preset-vaapi";
@@ -76,12 +81,12 @@
                 ffmpeg.inputs = [
                   {
                     "path" = "rtmp://user:password@192.168.87.22:1935";
-                    roles = ["rtmp"];
+                    roles = ["rtmp" "detect"];
                   }
                   {
                     "path" = "rtsp://user:password@192.168.87.22:554/h264Preview_01_main";
                     input_args = "preset-rtsp-restream";
-                    roles = ["record" "detect"];
+                    roles = ["record"];
                   }
                 ];
               };
