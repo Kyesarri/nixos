c632ab3c23ae0a487055a0f701aef6fb95825e3c
kye <kyesarri@gmail.com>
2024-03-30 12:43:03 +1100

    30.03.24 fix blocky container name

--- containers/blocky/default.nix
+++ containers/blocky/default.nix
@@ -1,41 +1,43 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}: {
-  containers.blocky = {
-    autoStart = true;
-    privateNetwork = true;
-    hostBridge = "br0";
-    localAddress = "192.168.87.2/24"; # container ip
+let
+  hostName = "blocky";
+  webPort = 80;
+in
+  {
+    config,
+    pkgs,
+    lib,
+    ...
+  }: {
+    containers.${hostName} = {
+      autoStart = true;
+      privateNetwork = true;
+      hostBridge = "br0";
+      localAddress = "192.168.87.2/24"; # container ip
 
-    config = {
-      config,
-      pkgs,
-      ...
-    }: let
-      hostName = "blocky";
-      webPort = 80;
-    in {
-      system.stateVersion = "23.11";
-
-      networking = {
-        hostName = "${hostName}";
-        domain = "home.lan";
-        nameservers = ["192.168.87.1"];
-        defaultGateway = "192.168.87.251";
-        firewall = {
-          enable = true;
-          allowedTCPPorts = [webPort];
+      config = {
+        config,
+        pkgs,
+        ...
+      }: {
+        system.stateVersion = "23.11";
+        security.acme.defaults.email = "kyesarri@gmail.com";
+        security.acme.acceptTerms = true;
+        networking = {
+          hostName = "${hostName}";
+          domain = "home.lan";
+          nameservers = ["192.168.87.1"];
+          defaultGateway = "192.168.87.251";
+          firewall = {
+            enable = true;
+            allowedTCPPorts = [webPort];
+          };
+          # workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
+          useHostResolvConf = lib.mkForce false;
         };
-        # workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
-        useHostResolvConf = lib.mkForce false;
+
+        # environment.systemPackages = with pkgs; [ffmpeg_5-full lshw];
+
+        services.blocky.enable = true;
       };
-
-      # environment.systemPackages = with pkgs; [ffmpeg_5-full lshw];
-
-      services.blocky.enable = true;
     };
-  };
-}
+  }
