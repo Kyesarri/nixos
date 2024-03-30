660091613218fb80365724bfc4a049b4e4bc2053
kye <kyesarri@gmail.com>
2024-03-30 12:10:41 +1100

    28.03.24 starting nginx container

---
+++ containers/nginx/default.nix
@@ -0,0 +1,42 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  containers.nginx = {
+    autoStart = true;
+    privateNetwork = true;
+    hostBridge = "br0";
+    localAddress = "192.168.87.44/24"; # container ip
+
+    config = {
+      config,
+      pkgs,
+      ...
+    }: let
+      hostName = "nginx";
+      webPort = 81;
+    in {
+      system.stateVersion = "23.11";
+
+      networking = {
+        hostName = "${hostName}";
+        domain = "home.lan";
+        nameservers = ["192.168.87.1"];
+        defaultGateway = "192.168.87.251";
+        firewall = {
+          enable = false;
+          allowedTCPPorts = [];
+        };
+        # workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
+        useHostResolvConf = lib.mkForce false;
+      };
+
+      # environment.systemPackages = with pkgs; [ffmpeg_5-full lshw];
+
+      services.resolved.enable = true;
+      services.nginx.enable = true;
+    };
+  };
+}
