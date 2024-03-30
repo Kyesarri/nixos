6a43ef784d2580f06186ec1a7513d03497dcefa6
kye <kyesarri@gmail.com>
2024-03-30 13:55:19 +1100

    30.03.24 add authelia base-config

--- containers/authelia/default.nix
+++ containers/authelia/default.nix
@@ -34,27 +34,12 @@ in
               secrets.storageEncryptionKeyFile = "/etc/authelia/storageEncryptionKeyFile";
               secrets.jwtSecretFile = "/etc/authelia/jwtSecretFile";
               settings = {
-                theme = "light";
+                theme = "dark";
                 default_2fa_method = "totp";
                 log.level = "debug";
                 server.disable_healthcheck = true;
               };
             };
-            preprod = {
-              enable = false;
-              secrets.storageEncryptionKeyFile = "/mnt/pre-prod/authelia/storageEncryptionKeyFile";
-              secrets.jwtSecretFile = "/mnt/pre-prod/jwtSecretFile";
-              settings = {
-                theme = "dark";
-                default_2fa_method = "webauthn";
-                server.host = "0.0.0.0";
-              };
-            };
-            test.enable = true;
-            test.secrets.manual = true;
-            test.settings.theme = "grey";
-            test.settings.server.disable_healthcheck = true;
-            test.settingsFiles = ["/mnt/test/authelia" "/mnt/test-authelia.conf"];
           };
         };
       };
