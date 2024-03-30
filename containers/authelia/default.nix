8befb4f77ac95ce5671cce3752d46a6602607099
kye <kyesarri@gmail.com>
2024-03-30 13:56:29 +1100

    30.03.24 add authelia base-config

--- containers/authelia/default.nix
+++ containers/authelia/default.nix
@@ -26,19 +26,16 @@ in
           enable = true;
           allowedTCPPorts = [webPort];
         };
-        services.authelia = {
-          enable = true;
-          instances = {
-            main = {
-              enable = true;
-              secrets.storageEncryptionKeyFile = "/etc/authelia/storageEncryptionKeyFile";
-              secrets.jwtSecretFile = "/etc/authelia/jwtSecretFile";
-              settings = {
-                theme = "dark";
-                default_2fa_method = "totp";
-                log.level = "debug";
-                server.disable_healthcheck = true;
-              };
+        services.authelia.instances = {
+          main = {
+            enable = true;
+            secrets.storageEncryptionKeyFile = "/etc/authelia/storageEncryptionKeyFile";
+            secrets.jwtSecretFile = "/etc/authelia/jwtSecretFile";
+            settings = {
+              theme = "dark";
+              default_2fa_method = "totp";
+              log.level = "debug";
+              server.disable_healthcheck = true;
             };
           };
         };
