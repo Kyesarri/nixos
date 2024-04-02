2103bb65ebe4d260a56b33def3bacedf42d96dc5
kye <kyesarri@gmail.com>
2024-04-03 08:59:32 +1100

    03.04.24 oci-containers test

--- home/frigate/default.nix
+++ home/frigate/default.nix
@@ -23,32 +23,35 @@
   environment.systemPackages = with pkgs; [docker-compose];
 
   # test.nix added here, see if we can run this as a systemd service
-  virtualisation.oci-containers.containers.frigate = {
-    # backend = "docker"; # or podman
-    hostname = "frigate";
-    autoStart = true;
-    image = "blakeblackshear/frigate:stable"; # or ghcr.io/blakeblackshear/frigate:stable
-    ports = [
-      "5000:5000"
-      "1935:1935"
-      "8554:8554" # rtsp
-      "8555:8555/tcp" # webrtc
-      "8555:8555/udp" # webrtc
-    ];
-    # environmentFiles = [ ../secrets/frigate.env ]; # TODO
-    volumes = [
-      "/home/${spaghetti.user}/.docker/frigate:/db"
-      "/home/${spaghetti.user}/.docker/frigate:/media/frigate"
-      "/home/${spaghetti.user}/.docker/frigate/config.yml:/config/config.yml:ro"
-      "/etc/localtime:/etc/localtime:ro"
-    ];
-    extraOptions = [
-      "--shm-size=64m"
-      # "--device=/dev/apex_0:/dev/apex_0" # dont have coral yet
-      "--device=/dev/dri/renderD128"
-      "--mount=type=tmpfs,target=/tmp/cache,tmpfs-size=1000000000"
-      "--pull=always"
-    ];
+  virtualisation.oci-containers = {
+    backend = "docker";
+    containers.frigate = {
+      # virtualisation.oci-containers.backend = "docker"; # or podman
+      hostname = "frigate";
+      autoStart = true;
+      image = "blakeblackshear/frigate:stable"; # or ghcr.io/blakeblackshear/frigate:stable
+      ports = [
+        "5000:5000"
+        "1935:1935"
+        "8554:8554" # rtsp
+        "8555:8555/tcp" # webrtc
+        "8555:8555/udp" # webrtc
+      ];
+      # environmentFiles = [ ../secrets/frigate.env ]; # TODO
+      volumes = [
+        "/home/${spaghetti.user}/.docker/frigate:/db"
+        "/home/${spaghetti.user}/.docker/frigate:/media/frigate"
+        "/home/${spaghetti.user}/.docker/frigate/config.yml:/config/config.yml:ro"
+        "/etc/localtime:/etc/localtime:ro"
+      ];
+      extraOptions = [
+        "--shm-size=64m"
+        # "--device=/dev/apex_0:/dev/apex_0" # dont have coral yet
+        "--device=/dev/dri/renderD128"
+        "--mount=type=tmpfs,target=/tmp/cache,tmpfs-size=1000000000"
+        "--pull=always"
+      ];
+    };
   };
 
   home-manager.users.${spaghetti.user} = {
