534775d6e5e9c6438020134b80f2f21809f7b970
kye <kyesarri@gmail.com>
2024-04-04 14:53:03 +1100

    04.04.24 secrets sssh

--- secrets/example.nix
+++ secrets/example.nix
@@ -1,6 +1,18 @@
 let
-  username = "ssh-foo bar";
-  users = [username];
+  # public keys added here
+  # rename to secrets.nix
+  #
+  # from https://github.com/ryantm/agenix
+  # This secrets.nix file is not imported into your NixOS configuration.
+  # It's only used for the agenix CLI tool (example below) to know which public keys to use for encryption.
+  user1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL0idNvgGiucWgup/mP78zyC23uFjYq0evcWdjGQUaBH";
+  user2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILI6jSq53F/3hEmSs+oq9L4TwOo1PrDMAgcA1uo1CCV/";
+  users = [user1 user2];
+
+  system1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPJDyIr/FSz1cJdcoW69R+NrWzwGK/+3gJpqD1t8L2zE";
+  system2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKzxQgondgEYcLpcPdJLrTdNgZ2gznOHCAxMdaceTUT1";
+  systems = [system1 system2];
 in {
-  "key.age".publicKeys = [username];
+  "secret1.age".publicKeys = [user1 system1];
+  "secret2.age".publicKeys = users ++ systems;
 }
