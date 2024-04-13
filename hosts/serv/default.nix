9baa60f31b161a5305e619ad3203274efaf284c2
kye <kyesarri@gmail.com>
2024-04-13 14:08:57 +1000

    12.04.23 serv haos container

--- hosts/serv/default.nix
+++ hosts/serv/default.nix
@@ -32,7 +32,7 @@
     ../../home/virt
     ../../home/gtk # has some theming bits, might have some requirement still
     ../../home/zsh # yes pls
-    ../../home/homeassistant
+    ../../home/home-assistant
 
     # ../../home/syncthing # testing without currently
     # ../../home/tailscale # TODO disabled until i can figure out mkif hostname == {nix-serv}; with an else
