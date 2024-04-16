aea33f8c8f8f38b3971575e4f81ccdbcb5054f5d
kye <kyesarri@gmail.com>
2024-04-16 22:54:56 +1000

    16.04.23 move oci to containers

--- containers/emqx/default.nix
+++ containers/emqx/default.nix
@@ -6,25 +6,30 @@
 }: {
   networking.firewall = {
     allowedTCPPorts = [1883 8083 8084 8883 18083];
-    allowedUDPPorts = [];
+    allowedUDPPorts = [18083];
   };
   virtualisation.oci-containers = {
     backend = "docker";
-    containers.emqx = {
-      hostname = "emqx-nix-serv";
-      autoStart = true;
-      image = "emqx:latest";
-      ports = [
-        "1883:1883"
-        "8083:8083"
-        "8084:8084"
-        "8883:8883"
-        "18083:18083"
-      ];
-      volumes = ["/home/${spaghetti.user}/.docker/emqx:/opt/emqx/data"];
-      environment = {
-        EMQX_NODE_NAME = "emqx@nix-serv";
-        EMQX_CLUSTER__DISCOVERY_STRATEGY = "static";
+    containers = {
+      emqx = {
+        hostname = "emqx-nix-serv";
+        autoStart = true;
+        image = "emqx:latest";
+        ports = [
+          "1883:1883"
+          "8083:8083"
+          "8084:8084"
+          "8883:8883"
+          "18083:18083"
+        ];
+        volumes = [
+          "/home/${spaghetti.user}/.docker/emqx:/opt/emqx/data"
+          "/etc/localtime:/etc/localtime:ro"
+        ];
+        environment = {
+          EMQX_NODE_NAME = "emqx@nix-serv";
+          EMQX_CLUSTER__DISCOVERY_STRATEGY = "static";
+        };
       };
     };
   };
