7b6c915bbb898d7e93721fbb90ed965b168695ed
kye <kyesarri@gmail.com>
2024-04-17 08:59:40 +1000

    17.04.23 dual mitx nic

--- containers/default.nix
+++ containers/default.nix
@@ -1,20 +1,3 @@
-{config, ...}:
-{
-  ## bridge
-  networking = {
-    bridges.br0.interfaces = ["enp1s0"]; # serv interface
-    useDHCP = false; # for host?
-    interfaces."br0".useDHCP = true; # bridged devices use dhcp by default
-    interfaces."br0".ipv4.addresses = [
-      {
-        address = "192.168.87.9"; # host ip
-        prefixLength = 24;
-      }
-    ];
-    defaultGateway = "192.168.87.251";
-    nameservers = ["1.1.1.1"];
-  };
-}
 {
   config,
   pkgs,
