21d8ca36a97a87ab86fe45781c463d6dcdd71861
kye <kyesarri@gmail.com>
2024-04-19 12:34:04 +1000

    19.04.24 more container networking | bridge test again

--- containers/arr/default.nix
+++ containers/arr/default.nix
@@ -1,51 +1,66 @@
 let
   hostName = "arr";
-  webPort = 80;
-  transmissionPort = 9091;
-  floodPort = 3000;
 in
   {
+    spaghetti,
     config,
     pkgs,
-    lib,
     ...
   }: {
-    networking.firewall.allowedTCPPorts = [webPort transmissionPort floodPort];
+    networking.firewall = {
+      allowedTCPPorts = [9091 7878];
+    };
 
-    containers.${hostName} = {
-      autoStart = true;
-      privateNetwork = true;
-      # hostAddress = "192.168.87.9";
-      hostBridge = "br0";
-      localAddress = "192.168.87.11/24";
-      config = {
-        config,
-        pkgs,
-        ...
-      }: {
-        system.stateVersion = "23.11";
-        services = {
-          resolved.enable = true;
-          transmission = {
-            enable = true;
-            user = "root";
-            # webHome = pkgs.flood;
-            performanceNetParameters = true;
-            openFirewall = true;
-            openRPCPort = true;
-            openPeerPorts = true;
-          };
+    virtualisation.oci-containers.containers = {
+      transmission = {
+        hostname = "transmission-nix-serv";
+        autoStart = true;
+        image = "linuxserver/transmission";
+        ports = [
+          "9091:9091"
+          "51413:51413"
+          "51413:51413/udp"
+        ];
+        volumes = [
+          "/home/${spaghetti.user}/.docker/transmission:/config"
+          # TODO "/path/to/downloads:/downloads"
+          # TODO "/path/to/watch/folder:/watch"
+          "/etc/localtime:/etc/localtime:ro"
+          "/etc/timezone:/etc/timezone:ro"
+        ];
+
+        environment = {
+          PUID = "1000";
+          PGID = "1000";
         };
 
-        networking = {
-          defaultGateway = "192.168.87.251";
-          useHostResolvConf = lib.mkForce false;
-          firewall = {
-            enable = true;
-            allowedTCPPorts = [webPort floodPort transmissionPort];
-          };
-        };
-        environment.systemPackages = with pkgs; [flood kitty];
+        extraOptions = [
+          "--network=host"
+          "--privileged"
+        ];
       };
+      #
+      /*
+      radarr = {
+        hostname = "radarr-nix-serv";
+        autoStart = true;
+        image = "lscr.io/linuxserver/radarr:latest";
+        ports = ["7878:7878"];
+        volumes = [
+          "/home/${spaghetti.user}/.docker/radarr:/config"
+          # TODO "/path/to/downloads:/downloads"
+          # TODO "/path/to/watch/folder:/watch"
+          "/etc/localtime:/etc/localtime:ro"
+          "/etc/timezone:/etc/timezone:ro"
+        ];
+
+          environment = {
+          PUID = 1000;
+          PGID = 1000;
+        };
+
+        extraOptions = [];
+      };
+      */
     };
   }
