9ca16b4cf110439bc5204861436ae757c474722e
kye <kyesarri@gmail.com>
2024-04-20 15:43:15 +1000

    20.04.24 transmission flood test

--- home/arr/flood.nix
+++ home/arr/flood.nix
@@ -10,6 +10,27 @@ in {
         postInstall =
           old.postInstall
           + ''
+            cp -R ${flood} $out/share/transmission/web/flood
+          '';
+      });
+    })
+  ];
+
+  systemd.services.transmission.environment.TRANSMISSION_WEB_HOME = "${pkgs.transmission}/share/transmission/web/flood";
+}
+/*
+{pkgs, ...}: let
+  flood = pkgs.fetchzip {
+    url = "https://github.com/johman10/flood-for-transmission/releases/download/2023-04-18T18-03-53/flood-for-transmission.zip";
+    hash = "sha256-alsHOTF8EEF7iUNHvLC21V3VRVoYQSs78g2r7YGTDeQ=";
+  };
+in {
+  nixpkgs.overlays = [
+    (self: super: {
+      transmission = super.transmission.overrideAttrs (old: {
+        postInstall =
+          old.postInstall
+          + ''
             rm -rf $out/share/transmission/web
             cp -R ${flood} $out/share/transmission/web/flood
           '';
@@ -17,3 +38,5 @@ in {
     })
   ];
 }
+*/
+
