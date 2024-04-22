310cf31f7c6a57cb87b865f00a66ff6d91abc5c2
kye <kyesarri@gmail.com>
2024-04-22 23:04:57 +1000

    22.04.24 refactor serv networking

--- containers/blocky/default.nix
+++ containers/blocky/default.nix
@@ -1,37 +1,41 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}: {
-  containers.${hostName} = {
-    autoStart = true;
-    privateNetwork = true;
-    hostBridge = "br0";
-    localAddress = "192.168.87.1/24";
+let
+  hostName = "blocky";
+  webPort = 80;
+in
+  {
+    config,
+    pkgs,
+    lib,
+    ...
+  }: {
+    containers.${hostName} = {
+      autoStart = true;
+      privateNetwork = true;
+      hostBridge = "br0";
+      localAddress = "192.168.87.1/24";
 
-    config = {
-      config,
-      pkgs,
-      ...
-    }: {
-      system.stateVersion = "23.11";
+      config = {
+        config,
+        pkgs,
+        ...
+      }: {
+        system.stateVersion = "23.11";
 
-      networking = {
-        hostName = "${hostName}";
-        # domain = "home.lan";
-        # nameservers = ["192.168.87.251"];
-        # defaultGateway = "192.168.87.251";
-        useHostResolvConf = lib.mkForce false;
-        firewall = {
-          enable = true;
-          allowedTCPPorts = [webPort];
+        networking = {
+          hostName = "${hostName}";
+          # domain = "home.lan";
+          # nameservers = ["192.168.87.251"];
+          # defaultGateway = "192.168.87.251";
+          useHostResolvConf = lib.mkForce false;
+          firewall = {
+            enable = true;
+            allowedTCPPorts = [webPort];
+          };
         };
+
+        # environment.systemPackages = with pkgs; [ffmpeg_5-full lshw];
+
+        services.blocky.enable = true;
       };
-
-      # environment.systemPackages = with pkgs; [ffmpeg_5-full lshw];
-
-      services.blocky.enable = true;
     };
-  };
-}
+  }
