402b9b43bd5ee35864779497b9b2b92467c3027f
kye <kyesarri@gmail.com>
2024-04-23 10:27:24 +1000

    23.04.24 refactor serv networking v3

--- containers/blocky/default.nix
+++ containers/blocky/default.nix
@@ -10,7 +10,6 @@ in
   }: {
     containers.${hostName} = {
       autoStart = true;
-      networkConfig.MACVLAN = "enp6s0";
       config = {
         config,
         pkgs,
@@ -18,24 +17,7 @@ in
       }: {
         system.stateVersion = "23.11";
         services.blocky.enable = true;
-        networking = {
-          hostName = "${hostName}";
-          useHostResolvConf = lib.mkForce false;
-          firewall = {
-            enable = true;
-            allowedTCPPorts = [webPort];
-          };
-        };
-        systemd.network = {
-          networks."10-mv-enp6s0" = {
-            matchConfig.Name = "mv-enp6s0";
-            address = ["192.168.87.2/24"];
-          };
-          netdevs."10-mv-enp6s0" = {
-            netdevConfig.Name = "mv-enp6s0";
-            netdevConfig.Kind = "veth";
-          };
-        };
+        networking.hostName = "${hostName}";
       };
     };
   }
