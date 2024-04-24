907b1b18b065590b18aed56bd28bbe648ab24555
kye <kyesarri@gmail.com>
2024-04-24 11:41:06 +1000

    24.04.24 test container networking v7

--- containers/blocky/default.nix
+++ containers/blocky/default.nix
@@ -11,7 +11,7 @@ in
       autoStart = true;
       privateNetwork = true;
       hostBridge = "20-mv-enp6s0-host";
-      # hostAddress = "192.168.87.99";
+      hostAddress = "192.168.87.99";
       localAddress = "192.168.87.2";
       config = {
         config,
