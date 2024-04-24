e5edc44cbeb003bcbc2af98a41add1f9fbc0fe91
kye <kyesarri@gmail.com>
2024-04-24 11:08:13 +1000

    24.04.24 test container networking v1

--- containers/blocky/default.nix
+++ containers/blocky/default.nix
@@ -11,7 +11,8 @@ in
     containers.${hostName} = {
       autoStart = true;
       privateNetwork = true;
-
+      hostAddress = "192.168.87.99";
+      localAddress = "192.168.87.2";
       config = {
         config,
         pkgs,
@@ -21,18 +22,22 @@ in
         services.blocky.enable = true;
         networking.hostName = "${hostName}";
         networking.useHostResolvConf = lib.mkForce false;
-
-        systemd.network = {
-          enable = true;
-          networks."10-mv-enp6s0" = {
-            matchConfig.Name = "mv-enp6s0";
-            address = ["192.168.87.2/24"];
-          };
-          netdevs."10-mv-enp6s0" = {
-            netdevConfig.Name = "mv-enp6s0";
-            netdevConfig.Kind = "veth";
-          };
-        };
       };
     };
   }
+/*
+    systemd.network = {
+      enable = true;
+      networks."10-mv-enp6s0" = {
+        matchConfig.Name = "mv-enp6s0";
+        address = ["192.168.87.2/24"];
+      };
+      netdevs."10-mv-enp6s0" = {
+        netdevConfig.Name = "mv-enp6s0";
+        netdevConfig.Kind = "veth";
+      };
+    };
+  };
+};
+*/
+
