097bb97e9c3ec912d6af5119169cc6f97230ead6
kye <kyesarri@gmail.com>
2024-04-24 22:41:41 +1000

    24.04.24

--- containers/nextcloud/default.nix
+++ containers/nextcloud/default.nix
@@ -6,17 +6,9 @@
 }: {
   ## currently testing the nextcloud container demo from
   ## https://nixos.wiki/wiki/NixOS_Containers
-  ## feels like i can leverage lots of this in /containers/default.nix
-  ## and leave per-container config in each /containers/container/default.nix
-
-  ## Below is nextcloud specific config
 
   containers.nextcloud = {
     autoStart = true;
-    privateNetwork = true;
-    hostAddress = "192.168.87.9/24";
-    # hostBridge = "br0";
-    localAddress = "192.168.87.8/24";
     config = {
       config,
       pkgs,
@@ -28,21 +20,20 @@
         resolved.enable = true;
         nextcloud = {
           enable = true;
-          package = pkgs.nextcloud28;
           hostName = "nextcloud";
           config.adminpassFile = "${pkgs.writeText "adminpass" "test123"}"; # DON'T DO THIS IN PRODUCTION - the password file will be world-readable in the Nix Store!
         };
       };
 
       networking = {
+        useDHCP = false;
+        useHostResolvConf = lib.mkForce false;
+        nameservers = ["192.168.87.251"];
         defaultGateway = "192.168.87.251";
         firewall = {
           enable = true;
           allowedTCPPorts = [80];
         };
-        # Use systemd-resolved inside the container
-        # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
-        useHostResolvConf = lib.mkForce false;
       };
     };
   };
