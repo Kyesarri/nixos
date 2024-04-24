c98ce695de23daa0c98f05382a40923c38df59d7
kye <kyesarri@gmail.com>
2024-04-25 09:00:14 +1000

    25.04.24 add coral frigate podman

--- containers/frigate/default.nix
+++ containers/frigate/default.nix
@@ -42,7 +42,7 @@
       ];
       extraOptions = [
         "--shm-size=256m"
-        # "--device=/dev/apex_0:/dev/apex_0" # dont have coral yet - implement 9900k as nix? yespls
+        "--device=/dev/apex_0:/dev/apex_0"
         "--device=/dev/dri/renderD128"
         "--mount=type=tmpfs,target=/tmp/cache,tmpfs-size=1000000000"
         "--pull=always"
