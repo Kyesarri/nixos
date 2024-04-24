9213bef89bd62e21e31a09ee4c06a94e79e16898
kye <kyesarri@gmail.com>
2024-04-24 20:42:08 +1000

    24.04.24

--- containers/nginx-proxy-manager/default.nix
+++ containers/nginx-proxy-manager/default.nix
@@ -12,7 +12,7 @@ in
       hostname = "${hostName}";
       autoStart = true;
       image = "docker.io/jc21/nginx-proxy-manager:latest";
-      ports = ["80:80" "192.168.87.1:81:81" "443:443"];
+      ports = ["80:80" "81:81" "443:443"];
       volumes = [
         "/etc/localtime:/etc/localtime:ro"
         "/home/${spaghetti.user}/.docker/${hostName}/data:/data"
