41c3dfa0f4ed1354d96c02ea65eadfd3e69144d8
kye <kyesarri@gmail.com>
2024-04-25 10:54:33 +1000

    25.04.24 add monitor serv

--- serv/monitor/default.nix
+++ serv/monitor/default.nix
@@ -1,27 +1,43 @@
 # taken parts from: https://xeiaso.net/blog/prometheus-grafana-loki-nixos-2020-11-20/
-# hosts/chrysalis/configuration.nix
-{
-  config,
-  pkgs,
-  ...
-}: {
-  # grafana configuration
-  networking.firewall.allowedTCPPorts = [2342];
-
-  services.grafana = {
-    enable = true;
-    settings.server = {
-      # domain = "grafana.home";
-      http_port = 2342;
-      http_addr = "0.0.0.0";
+let
+  serv = {
+    grafana = {
+      webPort = 2342;
+      httpAddr = "0.0.0.0";
+      domain = "grafana.home";
+    };
+    prometheus = {
+      port = 9001;
     };
   };
+in
+  {
+    config,
+    pkgs,
+    ...
+  }: {
+    # grafana configuration
+    networking.firewall.allowedTCPPorts = [serv.grafana.webPort serv.prometheus.port];
 
-  # nginx reverse proxy
-  services.nginx.virtualHosts.${config.services.grafana.domain} = {
-    locations."/" = {
-      proxyPass = "http://127.0.0.1:${toString config.services.grafana.port}";
-      proxyWebsockets = true;
+    services.grafana = {
+      enable = true;
+      settings.server = {
+        # domain = "${serv.grafana.domain}";
+        http_port = serv.grafana.webPort;
+        http_addr = "${serv.grafana.httpAddr}";
+      };
     };
-  };
-}
+
+    services.prometheus = {
+      enable = true;
+      port = serv.prometheus.port;
+    };
+
+    # nginx reverse proxy
+    services.nginx.virtualHosts.${config.services.grafana.domain} = {
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:${toString config.services.grafana.port}";
+        proxyWebsockets = true;
+      };
+    };
+  }
