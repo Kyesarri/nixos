d0a307e8060edcf56deebf954b6e4eb22e700335
kye <kyesarri@gmail.com>
2024-04-29 22:39:59 +1000

    29.04.24 test bridge erying

--- hosts/erying/networking.nix
+++ hosts/erying/networking.nix
@@ -30,13 +30,31 @@
       };
     };
 
+    "10-lan-bridge" = {
+      matchConfig.Name = "br0";
+      linkConfig.RequiredForOnline = "routable";
+      networkConfig = {
+        Address = ["192.168.87.30/24"];
+        Gateway = "192.168.87.251";
+        DNS = "1.1.1.1";
+        IPv6AcceptRA = true;
+      };
+    };
+
+    "20-eno1" = {
+      matchConfig.Name = "enp1s0";
+      networkConfig.Bridge = "br0";
+      linkConfig.RequiredForOnline = "enslaved";
+    };
+
+    /*
     networks."10-enp1s0:" = {
       matchConfig.Name = "enp1s0"; # 2.5g built-in
       address = ["192.168.87.30/24"];
       routes = [{routeConfig.Gateway = "192.168.87.251";}];
       linkConfig.RequiredForOnline = "routable";
     };
-
+    */
     /*
     networks."20-xxx:" = {
       matchConfig.Name = "xxx:"; # TODO - m.2 ethernet
