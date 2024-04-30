9c8e0b51ea8902b4f71faeca6d9d82950e8d9dfe
kye <kyesarri@gmail.com>
2024-04-30 16:03:45 +1000

    30.04.24 test bridge erying v3 day after

--- hosts/erying/networking.nix
+++ hosts/erying/networking.nix
@@ -1,6 +1,5 @@
 {
   config,
-  pkgs,
   lib,
   ...
 }: {
@@ -17,29 +16,68 @@
     };
   };
 
-  systemd.network = {
+  boot.initrd.systemd.network = {
     enable = true;
-    wait-online.enable = lib.mkForce false;
-
-    netdevs = {
-      # Create the bridge interface
-      "10-br0" = {
-        netdevConfig = {
-          Kind = "bridge";
-          Name = "br0";
-        };
-      };
-    };
-
-    networks."10-bridge" = {
-      matchConfig.Name = "br0";
+    networks."10-lan" = {
+      address = ["192.168.87.1/24"];
+      gateway = ["192.168.87.251"];
+      matchConfig.Name = ["enp1s0"];
       networkConfig = {
-        Address = ["192.168.87.1/24"];
-        Gateway = "192.168.87.251";
-        DNS = ["192.168.87.251"];
-        IPv6AcceptRA = true;
+        IPv6PrivacyExtensions = "yes";
+        MulticastDNS = true;
       };
       linkConfig.RequiredForOnline = "routable";
     };
   };
+
+  # Create a MACVTAP for ourselves too, so that we can communicate with
+  # our guests on the same interface.
+  systemd.network.netdevs."10-lan-self" = {
+    netdevConfig = {
+      Name = "lan-self";
+      Kind = "macvlan";
+    };
+    extraConfig = ''
+      [MACVLAN]
+      Mode=bridge
+    '';
+  };
+
+  systemd.network.networks = {
+    "10-lan" = {
+      matchConfig.MACAddress = config.repo.secrets.local.networking.interfaces.lan.mac;
+      # This interface should only be used from attached macvtaps.
+      # So don't acquire a link local address and only wait for
+      # this interface to gain a carrier.
+      networkConfig.LinkLocalAddressing = "no";
+      linkConfig.RequiredForOnline = "carrier";
+      extraConfig = ''
+        [Network]
+        MACVLAN=lan-self
+      '';
+    };
+    "20-lan-self" = {
+      address = ["192.168.87.1/24"];
+      gateway = ["192.168.87.251"];
+      matchConfig.Name = "lan-self";
+      networkConfig = {
+        IPv6PrivacyExtensions = "yes";
+        MulticastDNS = true;
+      };
+      linkConfig.RequiredForOnline = "routable";
+    };
+    # Remaining macvtap interfaces should not be touched.
+    "90-macvtap-ignore" = {
+      matchConfig.Kind = "macvtap";
+      linkConfig.ActivationPolicy = "manual";
+      linkConfig.Unmanaged = "yes";
+    };
+  };
+
+  networking.nftables.firewall = {
+    zones.untrusted.interfaces = ["lan-self"];
+  };
+
+  # Allow accessing influx
+  wireguard.proxy-sentinel.client.via = "sentinel";
 }
