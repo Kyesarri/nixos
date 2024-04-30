24b850b660ff07d94ce0140b52d192ee3ea18fe0
kye <kyesarri@gmail.com>
2024-04-30 23:55:15 +1000

    30.04.24 test throwing spaghetti at it... more

--- containers/default.nix
+++ containers/default.nix
@@ -5,6 +5,7 @@
       enable = true;
       autoPrune.enable = true;
       dockerCompat = true; # lets us use docker commands - translate to podman
+      defaultNetwork.settings.dns_enabled = true; # lets containers see each other
     };
   };
   environment.systemPackages = with pkgs; [
@@ -12,20 +13,19 @@
     podman-tui
     intel-gpu-tools # useful for monitoring igpu usage
   ];
-}
-/*
-# https://madison-technologies.com/take-your-nixos-container-config-and-shove-it/
-# we create a systemd service so that we can create a single "pod"
-# for our containers to live inside of. This will mimic how docker compose
-# creates one network for the containers to live inside of
-systemd.services.create-pod-network = with config.virtualisation.oci-containers; {
-  serviceConfig.Type = "oneshot";
-  wantedBy = ["${backend}-nginx-proxy-manager.service" "${backend}-frigate.service" "${backend}-home-assistant.service" "${backend}-plex.service"];
-  script = ''
-    ${pkgs.podman}/bin/podman network exists pod-net || \
-    ${pkgs.podman}/bin/podman network create pod-net
-  '';
-};
-*/
-# dont think this is required currently - if i want to isolate containers and have a proxy deal with connections this would be the deal
 
+  # https://madison-technologies.com/take-your-nixos-container-config-and-shove-it/
+  # we create a systemd service so that we can create a single "pod"
+  # for our containers to live inside of. This will mimic how docker compose
+  # creates one network for the containers to live inside of
+  /*
+  systemd.services.create-pod-network = with config.virtualisation.oci-containers; {
+    serviceConfig.Type = "oneshot";
+    wantedBy = ["${backend}-nginx-proxy-manager.service" "${backend}-frigate.service" "${backend}-home-assistant.service" "${backend}-plex.service"];
+    script = ''
+      ${pkgs.podman}/bin/podman network exists pod-net || \
+      ${pkgs.podman}/bin/podman network create pod-net
+    '';
+  };
+  */
+}
