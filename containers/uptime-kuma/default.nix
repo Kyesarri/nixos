1e6d7f959b286bd545321ac755d0bce4da173c93
kye <kyesarri@gmail.com>
2024-04-30 16:22:31 +1000

    30.04.24 test authelia erying

--- containers/uptime-kuma/default.nix
+++ containers/uptime-kuma/default.nix
@@ -14,7 +14,8 @@ in
       autoStart = true;
       privateNetwork = true;
       localAddress = "192.168.87.2/24";
-      hostBridge = "br0";
+      # hostBridge = "br0";
+      macvlans = ["enp1s0"];
       config = {
         config,
         pkgs,
