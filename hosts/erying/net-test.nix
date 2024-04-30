bf042b91a41922257168e174adbc2df261de59cd
kye <kyesarri@gmail.com>
2024-05-01 09:40:14 +1000

    01.05.24 test macvlan utk erying

--- hosts/erying/net-test.nix
+++ hosts/erying/net-test.nix
@@ -5,13 +5,13 @@
 }: {
   ## bridge
 
-  # networking.nat = {
-  #  enable = true;
-  #  internalInterfaces = ["ve-+"];
-  #  externalInterface = "enp1s0"; # moving to m.2 ethernet "soon"
-  #  # Lazy IPv6 connectivity for the container # do i want ipv6? :)
-  #  enableIPv6 = false;
-  # };
+  networking.nat = {
+    enable = true;
+    internalInterfaces = ["ve-+"];
+    externalInterface = "enp1s0"; # moving to m.2 ethernet "soon"
+    # Lazy IPv6 connectivity for the container # do i want ipv6? :)
+    #  enableIPv6 = false;
+  };
 
   networking = {
     hostName = "nix-erying";
