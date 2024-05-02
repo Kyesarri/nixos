01a2e25d99f74dc916824bf1411ffa55763de45c
kye <kyesarri@gmail.com>
2024-05-02 12:24:15 +1000

    02.05.24 + coral add pkgs.gasket to env

--- hardware/coral/default.nix
+++ hardware/coral/default.nix
@@ -8,6 +8,7 @@
   #gasket = config.boot.kernelPackages.callPackage /home/${spaghetti.user}nixos/hardware/coral/gasket.nix {};
 in {
   services.udev.packages = [libedgetpu];
+  environment.systemPackages = [pkgs.gasket];
   users.groups.plugdev = {};
   #boot.extraModulePackages = [gasket];
 }
