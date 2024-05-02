16843d8657cb1019c7ee0020b63c5b572497040a
kye <kyesarri@gmail.com>
2024-05-02 13:27:56 +1000

    02.05.24 codeproject add symlink - modulesettings, clean up comments in coral

--- containers/codeproject/default.nix
+++ containers/codeproject/default.nix
@@ -22,4 +22,29 @@ in
         "--device=/dev/apex_0:/dev/apex_0"
       ];
     };
+
+    home-manager.users.${spaghetti.user}.home.file.".docker/codeproject/data/modulesettings.json".text = ''
+            {
+        "Modules": {
+          "FaceProcessing": {
+            "LaunchSettings": {
+              "AutoStart": false
+            },
+            "GpuOptions": {}
+          },
+          "ObjectDetectionYOLOv5-6.2": {
+            "LaunchSettings": {
+              "AutoStart": false
+            },
+            "GpuOptions": {}
+          },
+          "ObjectDetectionCoral": {
+            "LaunchSettings": {
+              "AutoStart": true
+            },
+            "GpuOptions": {}
+          }
+        }
+      }
+    '';
   }
--- hardware/coral/default.nix
+++ hardware/coral/default.nix
@@ -5,11 +5,8 @@
   ...
 }: let
   libedgetpu = config.boot.kernelPackages.callPackage ./libedgetpu.nix {};
-  #gasket = config.boot.kernelPackages.callPackage /home/${spaghetti.user}nixos/hardware/coral/gasket.nix {};
 in {
   boot.extraModulePackages = [pkgs.linuxKernel.packages.linux_xanmod.gasket];
   services.udev.packages = [libedgetpu];
-  # environment.systemPackages = [pkgs.gasket]; # rm in unstable hense kernel packages
   users.groups.plugdev = {};
-  #boot.extraModulePackages = [gasket];
 }
