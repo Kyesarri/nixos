43066f8c2ec6723271c024c0ebb05e1bfa7744ec
kye <kyesarri@gmail.com>
2024-05-04 12:04:26 +1000

    04.05.23 codeproject being an arse, cannot fix atm

--- containers/codeproject/default.nix
+++ containers/codeproject/default.nix
@@ -7,13 +7,14 @@
 }: let
   hostName = "codeproject";
   webPort = 32168;
-  dir1 = "/home/${spaghetti.user}/.containers/${hostName}/usr/lib/x86_64-linux-gnu";
+  # dir1 = "/home/${spaghetti.user}/.containers/${hostName}/usr/lib/x86_64-linux-gnu";
   dir2 = "/home/${spaghetti.user}/.containers/${hostName}/etc/codeproject/ai";
   dir3 = "/home/${spaghetti.user}/.containers/${hostName}/app";
 in {
   system.activationScripts.makeCodeProjectDir = lib.stringAfter ["var"] ''
-    mkdir -p ${toString dir1} ${toString dir2} ${toString dir3} && echo volumes created for ${hostName}
-  '';
+    mkdir -p ${toString dir2} ${toString dir3} && echo volumes created for ${hostName}
+  ''; #    mkdir -p ${toString dir1} ${toString dir2} ${toString dir3} && echo volumes created for ${hostName}
+
   networking.firewall.allowedTCPPorts = [32168];
   virtualisation.oci-containers.containers.${hostName} = {
     hostname = "${hostName}-nix-serv";
@@ -22,7 +23,7 @@ in {
     ports = ["${toString webPort}:${toString webPort}"];
     volumes = [
       "/etc/localtime:/etc/localtime:ro"
-      "${toString dir1}:/usr/lib/x86_64-linux-gnu"
+      # "${toString dir1}:/usr/lib/x86_64-linux-gnu"
       "${toString dir2}:/etc/codeproject/ai"
       "${toString dir3}:/app"
     ];
