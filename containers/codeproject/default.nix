876ea8216cd0bcb0f21876b3279bdc1be0ac6504
kye <kyesarri@gmail.com>
2024-05-04 11:56:21 +1000

    04.05.23 more codeproject changes

--- containers/codeproject/default.nix
+++ containers/codeproject/default.nix
@@ -7,9 +7,9 @@
 }: let
   hostName = "codeproject";
   webPort = 32168;
-  dir1 = "/home/${spaghetti.user}/.containers/${hostName}/etc/codeproject/ai";
-  dir2 = "/home/${spaghetti.user}/.containers/${hostName}/app/modules";
-  dir3 = "/home/${spaghetti.user}/.containers/${hostName}/usr/lib/x86_64-linux-gnu";
+  dir1 = "/home/${spaghetti.user}/.containers/${hostName}/usr";
+  dir2 = "/home/${spaghetti.user}/.containers/${hostName}/etc/codeproject/ai";
+  dir3 = "/home/${spaghetti.user}/.containers/${hostName}/app";
 in {
   system.activationScripts.makeCodeProjectDir = lib.stringAfter ["var"] ''
     mkdir -p ${toString dir1} ${toString dir2} ${toString dir3} && echo volumes created for ${hostName}
@@ -22,9 +22,9 @@ in {
     ports = ["${toString webPort}:${toString webPort}"];
     volumes = [
       "/etc/localtime:/etc/localtime:ro"
-      "${toString dir1}:/etc/codeproject/ai"
-      "${toString dir2}:/app/modules"
-      "${toString dir3}:/usr/lib/x86_64-linux-gnu"
+      "${toString dir1}:/usr"
+      "${toString dir2}:/etc/codeproject/ai"
+      "${toString dir3}:/app"
     ];
     extraOptions = [
       "--device=/dev/apex_0:/dev/apex_0"
