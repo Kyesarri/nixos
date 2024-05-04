aa416fff35c61a355470225251de05c12181d4a3
kye <kyesarri@gmail.com>
2024-05-04 11:51:20 +1000

    04.05.23 more codeproject changes

--- containers/codeproject/default.nix
+++ containers/codeproject/default.nix
@@ -6,19 +6,20 @@
   ...
 }: let
   hostName = "codeproject";
+  webPort = 32168;
   dir1 = "/home/${spaghetti.user}/.containers/${hostName}/etc/codeproject/ai";
   dir2 = "/home/${spaghetti.user}/.containers/${hostName}/app/modules";
   dir3 = "/home/${spaghetti.user}/.containers/${hostName}/usr/lib/x86_64-linux-gnu";
 in {
   system.activationScripts.makeCodeProjectDir = lib.stringAfter ["var"] ''
-    mkdir -p ${toString dir1} ${toString dir2} ${toString dir3} && echo volumes created for ${toString hostName}
+    mkdir -p ${toString dir1} ${toString dir2} ${toString dir3} && echo volumes created for ${hostName}
   '';
   networking.firewall.allowedTCPPorts = [32168];
   virtualisation.oci-containers.containers.${hostName} = {
     hostname = "${hostName}-nix-serv";
     autoStart = true;
     image = "codeproject/ai-server:latest";
-    ports = ["32168:32168"];
+    ports = ["${toString webPort}:${toString webPort}"];
     volumes = [
       "/etc/localtime:/etc/localtime:ro"
       "${toString dir1}:/etc/codeproject/ai"
