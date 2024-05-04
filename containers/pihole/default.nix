942215c88c8fdc33807776e9ffe9765ee95b4da7
kye <kyesarri@gmail.com>
2024-05-04 12:49:31 +1000

    04.05.23 codeproject being an arse, cannot fix atm

--- containers/pihole/default.nix
+++ containers/pihole/default.nix
@@ -1,39 +1,39 @@
-let
+{
+  spaghetti,
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
   hostName = "pihole";
-in
-  {
-    spaghetti,
-    config,
-    pkgs,
-    lib,
-    ...
-  }: {
-    # looks to make directories on boot / rebuild - not sure if podman will handle this by itself?
-    # don't believe so, this is quite handy xoxo
-    system.activationScripts.makePiHoleDir = lib.stringAfter ["var"] ''
-      mkdir -p /home/${spaghetti.user}/.containers/${hostName}/etc /home/${spaghetti.user}/.containers/${hostName}/etc/dnsmasq.d
-    '';
+  webPort = 32168;
+  dir1 = "/home/${spaghetti.user}/.containers/${hostName}/etc/";
+  dir2 = "/home/${spaghetti.user}/.containers/${hostName}/etc/dnsmasq.d:";
+in {
+  system.activationScripts.makeCodeProjectDir = lib.stringAfter ["var"] ''
+    mkdir -p ${toString dir1} ${toString dir2} && echo volumes created for ${hostName}
+  '';
 
-    networking.firewall.allowedTCPPorts = [533 8080];
-    networking.firewall.allowedUDPPorts = [533 57 67];
+  networking.firewall.allowedTCPPorts = [53 8080];
+  networking.firewall.allowedUDPPorts = [53 67];
 
-    virtualisation.oci-containers.containers."${hostName}" = {
-      autoStart = true;
-      image = "pihole/pihole:latest";
-      ports = [
-        # "host:container"
-        "533:53/udp"
-        "533:53/tcp"
-        "67:67/udp"
-        "8080:80/tcp"
-      ];
-      volumes = [
-        "/home/${spaghetti.user}/.containers/${hostName}/etc:/etc/pihole"
-        "/home/${spaghetti.user}/.containers/${hostName}/etc/dnsmasq.d:/etc/dnsmasq.d"
-      ];
-      extraOptions = ["--cap-add=net_admin"];
-    };
-  }
+  virtualisation.oci-containers.containers."${hostName}" = {
+    autoStart = true;
+    image = "pihole/pihole:latest";
+    ports = [
+      # "host:container"
+      "53:53/udp"
+      "53:53/tcp"
+      "67:67/udp"
+      "8080:80/tcp"
+    ];
+    volumes = [
+      "${toString dir1}:/etc/pihole"
+      "${toString dir2}:/etc/dnsmasq.d"
+    ];
+    extraOptions = ["--cap-add=net_admin"];
+  };
+}
 # yoinked from https://gitlab.com/yramagicman/stow-dotfiles/-/blob/master/nixos/browncoat/pihole.nix?ref_type=heads
 # added my own tweaks
 
