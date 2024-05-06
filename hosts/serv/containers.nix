96dd5471de5d3535692a87d4a8e65f17fbdbe8e0
kye <kyesarri@gmail.com>
2024-05-06 11:26:13 +1000

    06.05.24 serv remove nginx proxy container

--- hosts/serv/containers.nix
+++ hosts/serv/containers.nix
@@ -6,7 +6,7 @@
     ../../containers/codeproject # oci
     # ../../containers/frigate # oci
     ../../containers/home-assistant # oci
-    ../../containers/nginx-proxy-manager # oci
+    # ../../containers/nginx-proxy-manager # oci
     ../../containers/pihole # oci
     ../../containers/plex # oci
   ];
