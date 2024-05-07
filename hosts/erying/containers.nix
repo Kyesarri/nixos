67ca833533feb89cf9092de04894ddee840a35ac
kye <kyesarri@gmail.com>
2024-05-08 09:41:05 +1000

    08.05.24 add pihole to erying

--- hosts/erying/containers.nix
+++ hosts/erying/containers.nix
@@ -8,23 +8,9 @@
     # ../../containers/authelia
     # ../../containers/uptime-kuma
     ../../containers/frigate
-    # ../../containers/pihole
+    ../../containers/pihole
   ];
-  /*
-  systemd.services.create-pod-net = {
-    description = "Start podman 'burrow' pod";
-    wants = ["network-online.target"];
-    after = ["network-online.target"];
-    wantedBy = ["podman-frigate.service"];
-    serviceConfig.Type = "oneshot";
-    #ExecStart = "-${pkgs.podman}/bin/podman pod create --driver=macvlan --gateway=192.168.87.251 --subnet=192.168.87.0/24 -o parent=enp3s0 burrow";
 
-    script = ''
-      ${pkgs.podman}/bin/podman network exists burrow || \
-        ${pkgs.podman}/bin/podman network create --driver=macvlan --gateway=192.168.87.251 --subnet=192.168.87.0/24 -o parent=enp3s0 burrow
-    '';
-  };
-  */
   systemd.services."podman-network-macvlan_lan" = {
     path = [pkgs.podman];
     wantedBy = ["podman-frigate.service"];
