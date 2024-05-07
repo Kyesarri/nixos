02ca618c4960bbeda96b1093136c05d8445cb824
kye <kyesarri@gmail.com>
2024-05-07 17:47:39 +1000

    07.05.24 love me some networking

--- hosts/erying/containers.nix
+++ hosts/erying/containers.nix
@@ -33,7 +33,7 @@
       ExecStop = "${pkgs.podman}/bin/podman network rm -f macvlan_lan";
     };
     script = ''
-      podman network exists macvlan_lan || podman network create --driver macvlan --opt parent=enp2s0 --subnet 192.168.1.0/24 --ip-range 192.168.1.224/27 --gateway 192.168.1.1 --route 224.0.0.0/4,192.168.1.1  macvlan_lan
+      podman network exists macvlan_lan || podman network create --driver macvlan --opt parent=enp3s0 --subnet 192.168.87.0/24 --ip-range 192.168.87.254/27 --gateway 192.168.87.251 --route 224.0.0.0/4,192.168.1.1  macvlan_lan
     '';
     partOf = ["podman-compose-root.target"];
     wantedBy = ["podman-compose-root.target"];
