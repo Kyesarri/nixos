2b46cef9735965b87b957e347df193e7ba78efeb
kye <kyesarri@gmail.com>
2024-05-07 17:01:54 +1000

    07.05.24 podman networking...

--- containers/default.nix
+++ containers/default.nix
@@ -1,16 +1,16 @@
 {pkgs, ...}: {
   virtualisation = {
-    oci-containers.backend = "podman"; # set podman as our default backend for containers
+    oci-containers.backend = "podman";
     podman = {
       enable = true;
       autoPrune.enable = true;
-      dockerCompat = true; # lets us use docker commands - translate to podman
-      defaultNetwork.settings.dns_enabled = true; # lets containers see each other
+      dockerCompat = true;
+      defaultNetwork.settings.dns_enabled = true;
     };
   };
   environment.systemPackages = with pkgs; [
     podman
     podman-tui
-    intel-gpu-tools # useful for monitoring igpu usage
+    intel-gpu-tools # intel igpu monitor
   ];
 }
