afca7e072d84c0da4249d47858764437df8d1c27
kye <kyesarri@gmail.com>
2024-05-08 10:50:06 +1000

    08.05.24 tweak pihole, change containers.nix erying host

--- hosts/erying/containers.nix
+++ hosts/erying/containers.nix
@@ -23,7 +23,7 @@
       ExecStop = "${pkgs.podman}/bin/podman network rm -f macvlan_lan";
     };
     script = ''
-      podman network exists macvlan_lan || podman network create --driver macvlan --opt parent=enp3s0 --subnet 192.168.87.0/24 --ip-range 192.168.87.254/24 --gateway 192.168.87.251 macvlan_lan
+      podman network exists macvlan_lan || podman network create --driver macvlan --opt parent=enp3s0 --subnet 192.168.87.0/24 --ip-range 192.168.87.255/24 --gateway 192.168.87.251 macvlan_lan
     '';
   };
 }
