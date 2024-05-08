ddbf0edfce5b541004b5d6f21e80544bdd1672b4
kye <kyesarri@gmail.com>
2024-05-09 09:44:46 +1000

    09.05.24 nukes didnt work

--- containers/pihole/default.nix
+++ containers/pihole/default.nix
@@ -19,11 +19,20 @@ in {
     autoStart = true;
     image = "pihole/pihole:latest";
     ports = [
-      "53:53/udp"
-      "53:53/tcp"
-      "67:67/udp"
-      "80:80/tcp"
+      #"53:53/udp"
+      #"53:53/tcp"
+      #"67:67/udp"
+      #"80:80/tcp"
     ];
+
+    environment = {
+      TZ = "Australia/Melbourne";
+      DNS1 = "1.1.1.1";
+      DNS2 = "8.8.8.8";
+      FTLCONF_LOCAL_IPV4 = "192.168.87.1";
+      #FTLCONF_LOCAL_IPV6 = "fd00::52e6:36ff:6496:f0f6";
+      #WEBPASSWORD = "nixpihole";
+    };
     volumes = [
       "/etc/localtime:/etc/localtime:ro"
       "${toString dir1}:/etc/pihole"
@@ -31,13 +40,12 @@ in {
     ];
 
     extraOptions = [
-      "--TZ=Australia/Melbourne"
       "--network=macvlan_lan"
-      "--ip=${secrets.ip.pihole}"
+      # "--ip=${secrets.ip.pihole}"
       "--pull=always"
       "--privileged"
-      "--FTLCONF_LOCAL_IPV4=${secrets.ip.pihole}"
-      "--dns=127.0.0.1"
+      # "--FTLCONF_LOCAL_IPV4=${secrets.ip.pihole}"
+      # "--dns=127.0.0.1"
     ];
   };
 }
