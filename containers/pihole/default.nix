86616eb850a3968370460b7b4aea0262f2c6e0e2
kye <kyesarri@gmail.com>
2024-05-09 09:58:34 +1000

    09.05.24 pihole working yay

--- containers/pihole/default.nix
+++ containers/pihole/default.nix
@@ -29,10 +29,9 @@ in {
       TZ = "Australia/Melbourne";
       DNS1 = "1.1.1.1";
       DNS2 = "8.8.8.8";
-      FTLCONF_LOCAL_IPV4 = "192.168.87.1";
-      #FTLCONF_LOCAL_IPV6 = "fd00::52e6:36ff:6496:f0f6";
-      #WEBPASSWORD = "nixpihole";
+      FTLCONF_LOCAL_IPV4 = "${secrets.ip.pihole}";
     };
+
     volumes = [
       "/etc/localtime:/etc/localtime:ro"
       "${toString dir1}:/etc/pihole"
@@ -41,11 +40,8 @@ in {
 
     extraOptions = [
       "--network=macvlan_lan"
-      # "--ip=${secrets.ip.pihole}"
       "--pull=always"
       "--privileged"
-      # "--FTLCONF_LOCAL_IPV4=${secrets.ip.pihole}"
-      # "--dns=127.0.0.1"
     ];
   };
 }
