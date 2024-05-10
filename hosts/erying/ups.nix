f5869c162c4e696082333e0688c99fb8dfceeb91
kye <kyesarri@gmail.com>
2024-05-10 10:48:24 +1000

    10.05.24 add ups to erying

--- hosts/erying/ups.nix
+++ hosts/erying/ups.nix
@@ -1,52 +1,17 @@
 {pkgs, ...}: {
   power.ups = {
     enable = true;
-    mode = "netserver";
-    ups = {
-      usbups = {
-        driver = "usbhid-ups";
-        port = "auto";
-        description = "USB UPS";
-        summary = ''
-          override.battery.charge.low = 33
-        '';
-      };
+
+    ups."cp600lcd" = {
+      driver = "usbhid-ups";
+      port = "auto";
     };
-  };
 
-  environment.etc = {
-    "nut/upsd.conf".source =
-      pkgs.writeText "upsd.conf"
-      ''
-        LISTEN 127.0.0.1 3493
-      '';
+    users.upsmon = {
+      passwordFile = "/path/to/upsmon.password";
+      upsmon = "master";
+    };
 
-    "nut/upsd.users".source =
-      pkgs.writeText "upsd.users"
-      ''
-        [upsmon]
-            password  = pass
-            upsmon primary
-            actions = set
-            actions = fsd
-            actions = test.panel.start
-            instcmds = ALL
-      '';
-
-    "nut/upsmon.conf".source =
-      pkgs.writeText "upsmon.conf"
-      ''
-        MONITOR usbups@localhost 1 upsmon pass primary
-        MINSUPPLIES 1
-        SHUTDOWNCMD "${pkgs.systemd}/bin/systemctl poweroff"
-        POLLFREQ 5
-        POLLFREQALERT 5
-        HOSTSYNC 15
-        DEADTIME 15
-        POWERDOWNFLAG /etc/killpower
-        RBWARNTIME 43200
-        NOCOMMWARNTIME 300
-        FINALDELAY 5
-      '';
+    upsmon.monitor."cp600lcd".user = "upsmon";
   };
 }
