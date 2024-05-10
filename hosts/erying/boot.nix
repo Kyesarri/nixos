bbf64a6bad585951db7a1877b70f4248201d2adc
kye <kyesarri@gmail.com>
2024-05-10 22:13:28 +1000

    10.05.24 erying - rm zfs extrapools

--- hosts/erying/boot.nix
+++ hosts/erying/boot.nix
@@ -3,7 +3,7 @@
     kernelPackages = pkgs.linuxPackages_xanmod; # backto mainline
     supportedFilesystems = ["zfs"]; # add zfs
     zfs.forceImportRoot = false;
-    zfs.extraPools = ["nvmea"]; # add new pools ere'
+    zfs.extraPools = [""]; # add new pools ere'
     kernelParams = [
       "intel_iommu=on" # pci device pass-through
       "nowatchdog" # disables watchdog, was causing shutdown / reboot issues on laptop, left in cos
