953a803b76c107ad43e72a088e9d7d3ea7467a31
kye <kyesarri@gmail.com>
2024-05-10 14:21:04 +1000

    10.05.24 emqx more testing

--- containers/emqx/default.nix
+++ containers/emqx/default.nix
@@ -8,11 +8,11 @@
 }: let
   contName = "emqx";
   dir1 = "/home/${spaghetti.user}/.containers/${contName}/data";
-  dir2 = "/home/${spaghetti.user}/.containers/${contName}/etc";
-  dir3 = "/home/${spaghetti.user}/.containers/${contName}/log";
+  dir2 = "/home/${spaghetti.user}/.containers/${contName}/log";
+  # dir3 = "/home/${spaghetti.user}/.containers/${contName}/etc";
 in {
   system.activationScripts.makeEMQXDir = lib.stringAfter ["var"] ''
-    mkdir -v -m 777 -p ${toString dir1} ${toString dir2} ${toString dir3}
+    mkdir -v -m 777 -p ${toString dir1} ${toString dir2}
   ''; # shitty perms, "temp" workaround
 
   virtualisation.oci-containers.containers.${contName} = {
@@ -22,11 +22,12 @@ in {
     volumes = [
       "/etc/localtime:/etc/localtime:ro"
       "${toString dir1}:/opt/emqx/data"
-      "${toString dir2}:/opt/emqx/etc"
-      "${toString dir3}:/opt/emqx/log"
+      "${toString dir2}:/opt/emqx/log"
+      #"${toString dir3}:/opt/emqx/etc"
     ];
     environment = {
-      #EMQX_NODE_NAME = "${contName}";
+      EMQX_HOST = "127.0.0.1";
+      EMQX_NAME = "${contName}";
     };
     extraOptions = [
       #"--pull=always"
