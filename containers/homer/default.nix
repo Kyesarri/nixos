cb2175fa9879267a60ce2519e4e2aad360a0d26e
kye <kyesarri@gmail.com>
2024-05-12 23:10:57 +1000

    12.05.24 add homer container

---
+++ containers/homer/default.nix
@@ -0,0 +1,33 @@
+{
+  spaghetti,
+  secrets,
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  contName = "homer";
+  dir1 = "/etc/oci.cont/${contName}";
+in {
+  system.activationScripts."make${contName}Dir" = lib.stringAfter ["var"] ''
+    mkdir -v -p ${toString dir1}
+  '';
+
+  virtualisation.oci-containers.containers."${contName}" = {
+    hostname = "${contName}";
+    autoStart = true;
+    image = "b4bz/homer:latest";
+
+    volumes = [
+      "/etc/localtime:/etc/localtime:ro"
+      "${toString dir1}:/www/assets"
+    ];
+
+    environment = {};
+
+    extraOptions = [
+      "--network=macvlan_lan"
+      "--ip=${secrets.ip.homer}"
+    ];
+  };
+}
