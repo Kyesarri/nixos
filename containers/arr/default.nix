c667d6c28344d3621f7990696d88fd267393a233
kye <kyesarri@gmail.com>
2024-05-15 22:28:54 +1000

    15.05.24 arr oci stack boilerplate configs

--- containers/arr/default.nix
+++ containers/arr/default.nix
@@ -1,34 +1,12 @@
-{
-  spaghetti,
-  secrets,
-  config,
-  pkgs,
-  lib,
-  ...
-}: let
-  contName = "arr";
-  dir1 = "/etc/oci.cont/${contName}";
-  # TODO = "lots";
-in {
-  system.activationScripts."make${contName}Dir" = lib.stringAfter ["var"] ''
-    mkdir -v -p ${toString dir1}
-  '';
-
-  virtualisation.oci-containers.containers."${contName}" = {
-    hostname = "${contName}";
-    autoStart = true;
-    image = "ghcr.io/home-assistant/home-assistant:stable";
-
-    volumes = [
-      "/etc/localtime:/etc/localtime:ro"
-      "${toString dir1}:/config"
-    ];
-
-    environment = {};
-
-    extraOptions = [
-      "--network=macvlan_lan"
-      "--ip=${secrets.ip.haos}"
-    ];
-  };
+{...}: {
+  # must be a better way to structure this, not a fan of
+  # so much boiler-plate for each container :D
+  imports = [
+    ./radarr.nix
+    ./sonarr.nix
+    ./bazarr.nix
+    ./proxarr.nix
+    ./prowlarr.nix
+    ./readarr.nix
+  ];
 }
---
+++ containers/arr/sonarr.nix
@@ -0,0 +1,33 @@
+{
+  spaghetti,
+  secrets,
+  # config,
+  # pkgs,
+  lib,
+  ...
+}: let
+  contName = "sonarr";
+  dir1 = "/etc/oci.cont/${contName}";
+in {
+  system.activationScripts."make${contName}Dir" = lib.stringAfter ["var"] ''
+    mkdir -v -p ${toString dir1} & chown 1000 ${toString dir1}
+  '';
+
+  virtualisation.oci-containers.containers."${contName}" = {
+    hostname = "${contName}";
+    autoStart = true;
+    image = "ghcr.io/linuxserver/${toString contName}:latest";
+
+    volumes = [
+      "/etc/localtime:/etc/localtime:ro"
+      "${toString dir1}:/config"
+    ];
+
+    environment = {};
+
+    extraOptions = [
+      # "--network=macvlan_lan"
+      # "--ip=${secrets.ip.haos}"
+    ];
+  };
+}
