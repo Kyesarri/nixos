e6a2c8f5613866ac0bf8c28a55c6c419a037a482
kye <kyesarri@gmail.com>
2024-05-16 11:39:42 +1000

    16.05.24 arr stack networking... love this

--- containers/arr/default.nix
+++ containers/arr/default.nix
@@ -25,11 +25,13 @@
       "${backend}-readarr.service"
     ];
     script = ''
-      ${pkgs.podman}/bin/podman pod exists arr_pod || ${pkgs.podman}/bin/podman pod create arr_pod
+      ${pkgs.podman}/bin/podman pod exists arr || \
+      ${pkgs.podman}/bin/podman pod create arr
     '';
     # podman network exists arr_net || podman network create --interface-name=arr_net --driver macvlan --opt parent=enp3s0 --subnet 10.1.1.0/24 --ip-range 10.1.1.255/24 --disable-dns=true arr_net
 
     # ${pkgs.podman}/bin/podman network create --driver macvlan --opt parent=<eth interface 2 here> --subnet 10.1.1.0/24 --ip-range 10.1.1.255/24 --gateway <insert proxarr ip here?> arr-net
+
     # this may need attention, only want proxarr to be between the podnet
     # and the local network, see how we progress
   };
