ce2e1121d0bd69f8c842975aed752b7dede38eb8
kye <kyesarri@gmail.com>
2024-05-17 13:37:41 +1000

    17.05.24 further tmux config

--- home/tmux/default.nix
+++ home/tmux/default.nix
@@ -2,17 +2,23 @@
   spaghetti,
   config,
   pkgs,
+  lib,
   ...
 }: {
   users.users.${spaghetti.user}.packages = [pkgs.tmuxifier];
 
   programs.tmux = {
-    enable = true;
     newSession = true; # spawn a new tmux session if trying to attach and none are running
-    shortcut = "b";
-    programs.tmux.plugins = [
+    enable = true;
+    aggressiveResize = true;
+    baseIndex = 1;
+    extraConfig = lib.readFile ./default.conf;
+    escapeTime = 0;
+    keyMode = "vi";
+    plugins = [
+      pkgs.tmuxPlugins.cpu
       pkgs.tmuxPlugins.tilish
-      pkgs.tmuxPlugins.jump
+      pkgs.tmuxPlugins.better-mouse-mode
     ];
   };
 }
