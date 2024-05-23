9ac829deca50f9d90fd4e8c489190c7ef7e5f922
kye <kyesarri@gmail.com>
2024-05-23 13:05:55 +1000

    23.05.24 fix cpai directories

--- containers/codeproject/default.nix
+++ containers/codeproject/default.nix
@@ -3,9 +3,9 @@
   lib,
   ...
 }: let
-  hostName = "cpai";
-  dir1 = "/etc/oci.cont/${contName}/work";
-  dir2 = "/etc/oci.cont/${contName}/conf";
+  contName = "cpai";
+  dir1 = "/etc/oci.cont/${contName}/etc/codeproject/ai";
+  dir2 = "/etc/oci.cont/${contName}/app/modules";
 in {
   system.activationScripts.makeCodeProjectDir = lib.stringAfter ["var"] ''
     mkdir -v -p ${toString dir1} ${toString dir2} & chown 1000:1000 ${toString dir1} & chown 1000:1000 ${toString dir2}
