adc797b892db397514f414e515b80b4dcbe59286
kye <kyesarri@gmail.com>
2024-06-21 09:22:06 +1000

    21.06.24 adding more storage to serv, update boot, ntfs support

--- hosts/serv/boot.nix
+++ hosts/serv/boot.nix
@@ -1,9 +1,15 @@
 {pkgs, ...}: {
   boot = {
     kernelPackages = pkgs.linuxPackages_xanmod; # use mainline xanmod kernel
-    supportedFilesystems = ["zfs"]; # add zfs, for storage
+    supportedFilesystems = ["zfs" "ntfs"];
     zfs.forceImportRoot = false;
-    zfs.extraPools = ["hddb" "hddc" "hddd" "hdde"];
+    zfs.extraPools = [
+      "hdda" # wdc 2tb
+      "hddb" # wdc 2tb
+      "hddc" # hgst 4tb
+      "hddd" # hgst 4tb
+      "hdde" # hgst 4tb
+    ];
     kernelParams = [
       # "i915.enable_fbc=1" # iGPU framebuffer compression, nfi if this works
       "intel_iommu=on" # pci device pass-through
