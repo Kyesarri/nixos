f0f358045ff3ee370920f4824287a745ceca3e3f
kye <kyesarri@gmail.com>
2024-06-25 13:53:23 +1000

    25.06.24 nut testing v1.1

--- serv/nut/default.nix
+++ serv/nut/default.nix
@@ -69,6 +69,54 @@ in {
       user = "nut";
     };
   };
+  upsmonConf = {
+    text = ''
+      MONITOR ${upsname}@127.0.0.1 1 monmaster "${pass-master}" master
+
+      RUN_AS_USER nut
+
+      MINSUPPLIES 1
+      SHUTDOWNCMD "/run/current-system/sw/bin/shutdown -h +0"
+      NOTIFYCMD /run/current-system/sw/bin/upssched
+      POLLFREQ 5
+      POLLFREQALERT 5
+      HOSTSYNC 15
+      DEADTIME 15
+      POWERDOWNFLAG /etc/killpower
+
+      NOTIFYMSG ONLINE  "UPS %s on line power"
+      NOTIFYMSG ONBATT  "UPS %s on battery"
+      NOTIFYMSG LOWBATT "UPS %s battery is low"
+      NOTIFYMSG FSD   "UPS %s: forced shutdown in progress"
+      NOTIFYMSG COMMOK  "Communications with UPS %s established"
+      NOTIFYMSG COMMBAD "Communications with UPS %s lost"
+      NOTIFYMSG SHUTDOWN  "Auto logout and shutdown proceeding"
+      NOTIFYMSG REPLBATT  "UPS %s battery needs to be replaced"
+      NOTIFYMSG NOCOMM  "UPS %s is unavailable"
+      NOTIFYMSG NOPARENT  "upsmon parent process died - shutdown impossible"
+
+      NOTIFYFLAG ONLINE SYSLOG+WALL+EXEC
+      NOTIFYFLAG ONBATT SYSLOG+WALL+EXEC
+      NOTIFYFLAG LOWBATT  SYSLOG+WALL+EXEC
+      NOTIFYFLAG FSD    SYSLOG+WALL+EXEC
+      NOTIFYFLAG COMMOK SYSLOG+WALL+EXEC
+      NOTIFYFLAG COMMBAD  SYSLOG+WALL+EXEC
+      NOTIFYFLAG SHUTDOWN SYSLOG+WALL+EXEC
+      NOTIFYFLAG REPLBATT SYSLOG+WALL+EXEC
+      NOTIFYFLAG NOCOMM SYSLOG+WALL+EXEC
+      NOTIFYFLAG NOPARENT SYSLOG+WALL
+
+      RBWARNTIME 43200
+
+      NOCOMMWARNTIME 300
+
+      FINALDELAY 5
+    '';
+    target = "nut/upsmon.conf";
+    mode = "0444";
+    group = "nut";
+    user = "nut";
+  };
 }
 /*
 Bus 001 Device 004: ID 047c:ffff Dell Computer Corp. UPS Tower 500W LV
