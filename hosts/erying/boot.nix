f56db9d27108483688bff93bfd2597ab8e526639
kye <kyesarri@gmail.com>
2024-07-15 15:53:47 +1000

    15.07.24 - serv rm grub - back to systemdboot, erying tidy task

--- hosts/erying/boot.nix
+++ hosts/erying/boot.nix
@@ -1,10 +1,11 @@
 {pkgs, ...}: {
   boot = {
+    kernelPackages = pkgs.linuxPackages_xanmod; # use mainline xanmod kernel
+    supportedFilesystems = ["zfs"];
     initrd.systemd.enable = true;
-    kernelPackages = pkgs.linuxPackages_xanmod; # backto mainline
-    supportedFilesystems = ["zfs"]; # add zfs
+
     zfs.forceImportRoot = false;
-    zfs.extraPools = []; # add new pools ere'
+    zfs.extraPools = [];
     kernelParams = [
       "intel_iommu=on" # pci device pass-through
       "nowatchdog" # disables watchdog, was causing shutdown / reboot issues on laptop, left in cos
--- hosts/serv/boot.nix
+++ hosts/serv/boot.nix
@@ -2,6 +2,8 @@
   boot = {
     kernelPackages = pkgs.linuxPackages_xanmod; # use mainline xanmod kernel
     supportedFilesystems = ["zfs" "ntfs"];
+    initrd.systemd.enable = true;
+
     zfs.forceImportRoot = false;
     zfs.extraPools = [
       "hdda" # wdc 2tb
@@ -15,19 +17,13 @@
       "hddi" # st 2tb
     ];
     kernelParams = [
-      # "i915.enable_fbc=1" # iGPU framebuffer compression, nfi if this works
       "intel_iommu=on" # pci device pass-through
       "nowatchdog" # disables watchdog, was causing shutdown / reboot issues on laptop, left in cos
     ];
 
     loader = {
-      efi.efiSysMountPoint = "/boot";
-      grub = {
-        enable = true;
-        efiSupport = true;
-        efiInstallAsRemovable = true; # otherwise /boot/EFI/BOOT/BOOTX64.EFI isn't generated
-        devices = ["nodev"];
-      };
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
     };
   };
 }
