4461e870f6a778e3e659a7425a74e46e753ec935
kye <kyesarri@gmail.com>
2024-07-31 00:07:10 +1000

    30.07.24 - infrared container - fix image :D

--- containers/minecraft/infrared.nix
+++ containers/minecraft/infrared.nix
@@ -6,7 +6,7 @@
   contName = "infrared";
   dir1 = "/etc/oci.cont/${contName}";
 in {
-  system.activationScripts."make${contName}Dir" = lib.stringAfter ["var"] ''mkdir -v -p ${toString dir1}'';
+  system.activationScripts."make${contName}Dir" = lib.stringAfter ["var"] ''mkdir -v -p ${toString dir1} '';
 
   # create symlinks in /etc - not sure if we can only write to paths relative to /etc
   # symlink file from nix tree to our container dir
@@ -15,7 +15,7 @@ in {
   virtualisation.oci-containers.containers."${contName}" = {
     hostname = "${contName}";
     autoStart = true;
-    image = "etc/infrared";
+    image = "haveachin/infrared:latest";
 
     environment = {
       domain = "${toString secrets.domain.minecraft}";
