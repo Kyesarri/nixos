310211e2a9370e99ea4985f59b5d66dfad77f18f
kye <kyesarri@gmail.com>
2024-08-03 12:47:19 +1000

    03.08.24 - update homer-wan container - testing source file vs text

--- containers/homer-wan/default.nix
+++ containers/homer-wan/default.nix
@@ -11,91 +11,10 @@ in {
   '';
 
   environment.etc."oci.cont/${contName}/config.yml" = {
-    mode = "644";
-    uid = 1000;
-    gid = 1000;
-    text = ''
-      ---
-      # Homepage configuration
-      # See https://fontawesome.com/v5/search for icons options
-
-      title: "yur.mom"
-      subtitle: "now on the internet"
-
-      header: true
-      footer: '<p> (づ ￣ ³￣)づ </p>'
-
-      connectivityCheck: false
-      defaults:
-        layout: list
-        colorTheme: dark
-
-      links:
-        - name: "homer - this page"
-          icon: "fab fa-github"
-          url: "https://github.com/bastienwirtz/homer"
-        - name: "my nixos configs"
-          icon: "fas fa-book"
-          url: "https://codeberg.org/kye/nixos"
-        # this will link to a second homer page that will load config from page2.yml and keep default config values as in config.yml file
-        # see url field and assets/page.yml used in this example:
-        # - name: "Second Page"
-        #   icon: "fas fa-file-alt"
-        #   url: "#page2"
-
-      theme: neon
-      colors:
-        light:
-          highlight-primary: "#E3E6EE" #
-          highlight-secondary: "#B072D1" # banner divider, card tags
-          highlight-hover: "#1C1E26" #
-          background: "#E3E6EE" #
-          card-background: "#DCDFE4" #
-          text: "#1C1E26" #
-          text-header: "#1C1E26" #
-          text-title: "#1C1E26" #
-          text-subtitle: "#1C1E26" #
-          card-shadow: rgb(228, 163, 130, 0.7) # #24A8B4 also used on footer
-          link: "#DF5273" # used for icons
-          link-hover: "#E93C58" # hover icons
-        dark:
-          highlight-primary: "#232530" #
-          highlight-secondary: "#B072D1" # banner divider, card tags
-          highlight-hover: "#1C1E26" #
-          background: "#1C1E26" #
-          card-background: "#232530" #
-          text: "#DCDFE4" #
-          text-header: "#DCDFE4" #
-          text-title: "#DCDFE4" #
-          text-subtitle: "#DCDFE4" #
-          card-shadow: rgb(228, 163, 130, 0.7) # #24A8B4 also used on footer
-          link: "#DF5273" # used for icons
-          link-hover: "#E93C58" # hover icons
-
-      services:
-        - name: "her things"
-          icon: "fas fa-hand-holding-heart fa-fw"
-
-          items:
-            - name: "plex"
-              icon: "fas fa-tv"
-              subtitle: "mom's special movies"
-              tag: "plex"
-              url: "https://www.yur.mom/plex"
-
-            - name: "request"
-              icon: "fas fa-hand-middle-finger"
-              subtitle: "now taking requests"
-              tag: "overseerr"
-              url: "https://www.yur.mom/request"
-
-            - name: "minecraft"
-              icon: "fas fa-cubes"
-              subtitle: "break some bricks"
-              tag: "minecraft"
-              type: "CopyToClipboard"
-              clipboard: "www.yur.mom"
-    '';
+    mode = "644"; # this fixed heaps of my issues <3
+    uid = 1000; # set uid
+    gid = 1000; # set gid
+    source = ./config.yml; # source file
   };
 
   virtualisation.oci-containers.containers."${contName}" = {
