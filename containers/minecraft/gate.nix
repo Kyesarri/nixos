495134d3120dfb873c64ab4393947f09c33df847
kye <kyesarri@gmail.com>
2024-08-04 23:59:49 +1000

    03.08.24 - update secrets

--- containers/minecraft/gate.nix
+++ containers/minecraft/gate.nix
@@ -22,7 +22,7 @@ in {
 
     volumes = [
       "/etc/localtime:/etc/localtime:ro"
-      "${toString dir1}/config.yml:config.yml"
+      "${toString dir1}/config.yml:/config.yml"
     ];
 
     extraOptions = [
