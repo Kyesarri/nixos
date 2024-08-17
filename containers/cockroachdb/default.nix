b324bdfe2d2064706608bd5a273b51472905968e
kye <kyesarri@gmail.com>
2024-08-18 09:44:34 +1000

    18.08.24 - containers/zitadel & containers/cockroachdb - added db to zitadel container, cockroachdb test example created

--- containers/cockroachdb/default.nix
+++ containers/cockroachdb/default.nix
@@ -1,17 +1,41 @@
-{}
-/*
-  cockroachdb/cockroach:latest
+# cockroachdb - see container zitadel
+{
+  lib,
+  secrets,
+  ...
+}: let
+  contName = "cockroachdb";
+  dir1 = "/etc/oci.cont/${contName}/.data";
+in {
+  system.activationScripts."make${contName}Dir" =
+    lib.stringAfter ["var"]
+    ''mkdir -v -p ${toString dir1}''; # & chown 1000:1000 ${toString dir1}
 
-docker run -d \
-  --env COCKROACH_DATABASE={DATABASE_NAME} \
-  --env COCKROACH_USER={USER_NAME} \
-  --env COCKROACH_PASSWORD={PASSWORD} \
-  --name=roach-single \
-  --hostname=roach-single \
-  -p 26257:26257 \
-  -p 8080:8080 \
-  -v "roach-single:/cockroach/cockroach-data"  \
-  cockroachdb/cockroach:v24.2.0 start-single-node \
-  --http-addr=roach-single:8080
-*/
+  virtualisation.oci-containers.containers."${contName}" = {
+    hostname = "${contName}";
+    autoStart = true;
+    image = "cockroachdb/cockroach:latest";
 
+    volumes = [
+      "/etc/localtime:/etc/localtime:ro"
+      "${toString dir1}:/cockroach/cockroach-data"
+    ];
+
+    cmd = ["start-single-node"];
+
+    environment = {
+      COCKROACH_DATABASE = "";
+      COCKROACH_USER = "";
+      COCKROACH_PASSWORD = "";
+      PUID = "1000";
+      PGID = "1000";
+      TZ = "Australia/Melbourne";
+    };
+
+    extraOptions = [
+      "--network=macvlan_lan"
+      "--network=zitadel"
+      "--ip=${secrets.ip.cockroachdb}"
+    ];
+  };
+}
--- containers/zitadel/default.nix
+++ containers/zitadel/default.nix
@@ -6,11 +6,12 @@
   contName = "zitadel";
   dir1 = "/etc/oci.cont/${contName}/";
 in {
-  system.activationScripts."make${contName}Dir" = lib.stringAfter ["var"] ''
-    mkdir -v -p ${toString dir1}
-  '';
+  system.activationScripts."make${contName}Dir" =
+    lib.stringAfter ["var"]
+    ''mkdir -v -p ${toString dir1} & ${toString dir1}-db'';
 
   virtualisation.oci-containers.containers = {
+    # zitadel
     "${contName}" = {
       hostname = "${contName}";
       autoStart = true;
@@ -23,13 +24,40 @@ in {
       environment = {
         ZITADEL_MASTERKEY = "${toString secrets.keys.zitadel}";
         TZ = "Australia/Melbourne";
-        ZITADEL_DATABASE_COCKROACH_HOST = "${toString secrets.ip.zitadel}";
-        ZITADEL_DATABASE_COCKROACH_PORT = "26257";
+        # ZITADEL_DATABASE_COCKROACH_HOST = "";
+        # ZITADEL_DATABASE_COCKROACH_PORT = "26257";
       };
 
       extraOptions = [
-        "--network=macvlan_lan"
-        "--ip=${secrets.ip.zitadel}"
+        "--network=zitadel"
+        "--network=macvlan_lan --ip=${secrets.ip.zitadel}"
+      ];
+    };
+
+    # zitadel-db
+    "${contName}-db" = {
+      hostname = "${contName}-db";
+      autoStart = true;
+      image = "cockroachdb/cockroach:latest";
+
+      volumes = [
+        "/etc/localtime:/etc/localtime:ro"
+        "${toString dir1}:/cockroach/cockroach-data"
+      ];
+
+      cmd = ["start-single-node"];
+
+      environment = {
+        COCKROACH_DATABASE = "${toString secrets.zitadel.dbname}";
+        COCKROACH_USER = "${toString secrets.zitadel.dbuser}";
+        COCKROACH_PASSWORD = "${toString secrets.zitadel.dbpass}";
+        PUID = "1000";
+        PGID = "1000";
+        TZ = "Australia/Melbourne";
+      };
+
+      extraOptions = [
+        "--network=zitadel"
       ];
     };
   };
