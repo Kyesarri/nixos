93a6e13707ee089c07127893415513268e0ccc78
kye <kyesarri@gmail.com>
2024-08-17 23:15:54 +1000

    17.08.24 - zitadel container in testing condition

--- containers/zitadel/default.nix
+++ containers/zitadel/default.nix
@@ -4,10 +4,10 @@
   ...
 }: let
   contName = "zitadel";
-  dir1 = "/etc/oci.cont/${contName}/.data";
+  dir1 = "/etc/oci.cont/${contName}/";
 in {
   system.activationScripts."make${contName}Dir" = lib.stringAfter ["var"] ''
-    mkdir -v -p ${toString dir1} & chown 1000:1000 ${toString dir1}
+    mkdir -v -p ${toString dir1}
   '';
 
   virtualisation.oci-containers.containers = {
@@ -16,26 +16,18 @@ in {
       autoStart = true;
       image = "ghcr.io/zitadel/zitadel:latest";
 
-      volumes = [
-        "/etc/localtime:/etc/localtime:ro"
-      ];
+      volumes = ["/etc/localtime:/etc/localtime:ro"];
 
-      cmd = [
-        "start-from-init"
-        "--masterkey /"
-        "MasterkeyNeedsToHave32Characters/"
-        "--tlsMode disabled"
-      ];
+      # using temp masterkey ;)
+      cmd = ["start-from-init --masterkey \"CZOjWCFaxeLUdwb1TjvmMFyS8j9ICQNY\" --tlsMode disabled"];
 
       environment = {
-        PUID = "1000";
-        PGID = "1000";
         TZ = "Australia/Melbourne";
       };
 
       extraOptions = [
         "--network=macvlan_lan"
-        "--ip=${secrets.ip.zigbee2mqtt}"
+        "--ip=${secrets.ip.zitadel}"
       ];
     };
   };
