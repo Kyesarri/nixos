900fd0c654551e6cc4e6d3622e2a13e79302d080
kye <kyesarri@gmail.com>
2024-08-19 14:48:33 +1000

    19.08.24 - containers/ustreamer - add ustreamer

---
+++ containers/ustreamer/default.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  secrets,
+  ...
+}: let
+  contName = "ustreamer";
+  dir1 = "/dev/video0:/dev/video0";
+in {
+  system.activationScripts."make${contName}Dir" = lib.stringAfter ["var"] ''
+    mkdir -v -p ${toString dir1}
+  '';
+
+  virtualisation.oci-containers.containers."${contName}" = {
+    hostname = "${contName}";
+    autoStart = true;
+    image = "mkuf/ustreamer:latest";
+
+    cmd = ["--host=0.0.0.0" "--port=80" "-f 30"];
+
+    volumes = [
+      "/etc/localtime:/etc/localtime:ro"
+      "${toString dir1}"
+    ];
+
+    environment = {
+      PUID = "1000";
+      PGID = "1000";
+      TZ = "Australia/Melbourne";
+    };
+
+    extraOptions = [
+      "--network=macvlan_lan"
+      "--ip=${secrets.ip.ustreamer}"
+    ];
+  };
+}
