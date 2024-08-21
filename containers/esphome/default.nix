fae2f08c6719d2bf7f788cac833eeeb8f2ecd854
kye <kyesarri@gmail.com>
2024-08-22 01:03:47 +1000

    22.08.24 esphome - change to stable image

--- containers/esphome/default.nix
+++ containers/esphome/default.nix
@@ -9,7 +9,7 @@ in {
   system.activationScripts.makeESPHomeDir = lib.stringAfter ["var"] ''mkdir -v -p ${toString dir1} & chown 1000:1000 ${toString dir1}'';
 
   virtualisation.oci-containers.containers.${contName} = {
-    image = "ghcr.io/esphome/esphome";
+    image = "ghcr.io/esphome/esphome:stable";
     autoStart = true;
     volumes = [
       "${toString dir1}:/config"
