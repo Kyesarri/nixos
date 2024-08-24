8937c98bbbce8e4a3069a0eedd5c144499ccab98
kye <kyesarri@gmail.com>
2024-08-24 13:33:03 +1000

    24.08.24 - containers/peanut - testing

--- containers/peanut/default.nix
+++ containers/peanut/default.nix
@@ -1,31 +1,22 @@
-{
-  secrets,
-  lib,
-  ...
-}: let
+{secrets, ...}: let
   contName = "peanut";
-  dir1 = "/etc/oci.cont/${contName}/config";
 in {
-  system.activationScripts.makeCodeProjectDir = lib.stringAfter ["var"] ''mkdir -v -p ${toString dir1} & chown 1000:1000 ${toString dir1}'';
-
   virtualisation.oci-containers.containers.${contName} = {
     hostname = "${contName}";
     autoStart = true;
     image = "brandawg93/peanut:latest";
 
-    volumes = [
-      "/etc/localtime:/etc/localtime:ro"
-      "${toString dir1}:/app/config"
-    ];
+    volumes = ["/etc/localtime:/etc/localtime:ro"];
 
     environment = {
       PUID = "1000";
       PGID = "1000";
-      NUT_HOST = "${secrets.ip.erying}";
+      NUT_HOST = "${toString secrets.ip.erying}";
       NUT_PORT = "3493";
       WEB_PORT = "8080";
       USERNAME = "upsmon";
       PASSWORD = "upsmon_pass";
+      WEB_HOST = "localhost";
     };
 
     extraOptions = [
