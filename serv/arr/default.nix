71ac04f6a4b96c43e2366c3baa131bc7252b8d2e
kye <kyesarri@gmail.com>
2024-09-01 02:21:37 +1000

    31.08.24 - add media group to services.arr...

--- serv/arr/default.nix
+++ serv/arr/default.nix
@@ -43,22 +43,27 @@
     radarr = {
       enable = true;
       openFirewall = true;
+      group = "media";
     };
     sonarr = {
       enable = true;
       openFirewall = true;
+      group = "media";
     };
     prowlarr = {
       enable = true;
       openFirewall = true;
+      group = "media";
     };
     readarr = {
       enable = true;
       openFirewall = true;
+      group = "media";
     };
     bazarr = {
       enable = true;
       openFirewall = true;
+      group = "media";
     };
   };
 }
