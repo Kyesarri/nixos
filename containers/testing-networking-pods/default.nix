0db2e70265775787ff26f7e16871f6ba9594afe9
kye <kyesarri@gmail.com>
2024-09-02 13:42:41 +1000

    02.09.24 - rollback flake.lock - issues with hyprland, add testing container erying

--- containers/testing-networking-pods/default.nix
+++ containers/testing-networking-pods/default.nix
@@ -25,7 +25,7 @@
   */
   virtualisation.oci-containers.containers = {
     nextcloud = {
-      image = "nextcloud:local";
+      image = "nextcloud:latest";
       autoStart = true;
       user = "1000:1000";
       dependsOn = ["mariadb" "redis"];
