eeb4cc718fae547a4aef12deeb068e9d79c80acb
kye <kyesarri@gmail.com>
2024-09-08 01:47:36 +1000

    08.09.24 - add erying disk labels (pls no break things)

--- hosts/erying/hardware.nix
+++ hosts/erying/hardware.nix
@@ -7,6 +7,8 @@
 }: {
   imports = [(modulesPath + "/installer/scan/not-detected.nix")];
 
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
   hardware = {
     cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
     enableRedistributableFirmware = lib.mkDefault true;
@@ -29,26 +31,31 @@
     "/" = {
       device = "/dev/disk/by-uuid/7f98b95a-b4a3-4a9c-94e8-76d77054fb28";
       fsType = "ext4";
+      label = "root";
     };
     "/boot" = {
       device = "/dev/disk/by-uuid/3847-FA30";
       fsType = "vfat";
       options = ["fmask=0022" "dmask=0022"];
+      label = "boot";
     };
     # 1tb nvme
     "/etc/oci.cont" = {
       device = "/dev/disk/by-uuid/2c81efe8-b0bc-4942-92c0-beff70cebca2";
       fsType = "ext4";
+      label = "containers";
     };
     # 1tb sata ssd
     "/etc/oci.cont.scratch" = {
       device = "/dev/disk/by-uuid/91b3bbfd-69c6-4081-ab4d-c1a9818be9b4";
       fsType = "ext4";
+      label = "scratch";
     };
     # 128gb nvme
     "/etc/oci.cont.nvme" = {
       device = "/dev/disk/by-uuid/49f27562-6797-4094-a12c-60f1e8d2c7f5";
       fsType = "ext4";
+      label = "nvmecontainers";
     };
   };
   #
@@ -58,6 +65,4 @@
       size = 16 * 1024; # 16GB
     }
   ];
-
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
