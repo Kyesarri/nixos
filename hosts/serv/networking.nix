45b55a2db155e721c0e9ef945c461d11ba2004cf
kye <kyesarri@gmail.com>
2024-09-17 09:17:30 +1000

    17.09.24 - update serv networking, pending rollback due to me messing up networking again, add comments to flake.nix for possible future use

--- hosts/serv/networking.nix
+++ hosts/serv/networking.nix
@@ -29,27 +29,53 @@
 
   boot.initrd.systemd.network = {
     enable = true;
-    networks."10-lan" = {
-      address = ["${toString secrets.ip.serv-1}/24"];
-      gateway = ["${toString secrets.ip.gateway}"];
-      matchConfig.Name = ["eno1"];
-      networkConfig = {
-        IPv6PrivacyExtensions = "yes";
-        MulticastDNS = true;
+    networks = {
+      "10-lan" = {
+        address = ["${toString secrets.ip.serv-1}/24"];
+        gateway = ["${toString secrets.ip.gateway}"];
+        matchConfig.Name = ["eno1"];
+        linkConfig.RequiredForOnline = "routable";
+        networkConfig = {
+          IPv6PrivacyExtensions = "yes";
+          MulticastDNS = true;
+        };
       };
-      linkConfig.RequiredForOnline = "routable";
+      # wait for this to break the internet
+      "30-vlan" = {
+        address = ["${toString secrets.ip.serv-2}/24"];
+        gateway = ["${toString secrets.ip.gateway}"];
+        matchConfig.Name = ["enp4s0"];
+        linkConfig.RequiredForOnline = "routable";
+        networkConfig = {
+          IPv6PrivacyExtensions = "yes";
+          MulticastDNS = true;
+        };
+      };
     };
   };
 
-  systemd.network.netdevs."10-lan-self" = {
-    netdevConfig = {
-      Name = "lan-self";
-      Kind = "macvlan";
+  systemd.network.netdevs = {
+    "10-lan-self" = {
+      netdevConfig = {
+        Name = "lan-self";
+        Kind = "macvlan";
+      };
+      extraConfig = ''
+        [MACVLAN]
+        Mode=bridge
+      '';
     };
-    extraConfig = ''
-      [MACVLAN]
-      Mode=bridge
-    '';
+
+    "30-vlan-self" = {
+      netdevConfig = {
+        Name = "vlan-self";
+        Kind = "macvlan";
+      };
+      extraConfig = ''
+        [MACVLAN]
+        Mode=bridge
+      '';
+    };
   };
 
   systemd.network.networks = {
@@ -62,15 +88,34 @@
         MACVLAN=lan-self
       '';
     };
+    "30-vlan" = {
+      matchConfig.Name = ["enp4s0"];
+      networkConfig.LinkLocalAddressing = "no";
+      linkConfig.RequiredForOnline = "carrier";
+      extraConfig = ''
+        [Network]
+        MACVLAN=vlan-self
+      '';
+    };
     "20-lan-self" = {
       address = ["${toString secrets.ip.serv-1}/24"];
       gateway = ["${toString secrets.ip.gateway}"];
       matchConfig.Name = "lan-self";
+      linkConfig.RequiredForOnline = "routable";
       networkConfig = {
         IPv6PrivacyExtensions = "yes";
         MulticastDNS = true;
       };
+    };
+    "30-vlan-self" = {
+      address = ["${toString secrets.ip.serv-2}/24"];
+      gateway = ["${toString secrets.ip.gateway}"];
+      matchConfig.Name = "vlan-self";
       linkConfig.RequiredForOnline = "routable";
+      networkConfig = {
+        IPv6PrivacyExtensions = "yes";
+        MulticastDNS = true;
+      };
     };
   };
 }
