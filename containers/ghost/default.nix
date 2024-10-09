e0fb97adab95ee6135a7fb13c8ea440194e14c81
kye <kyesarri@gmail.com>
2024-10-09 17:36:30 +1100

    09.10.24 - update ghost container

--- containers/ghost/default.nix
+++ containers/ghost/default.nix
@@ -23,7 +23,7 @@ in {
       database__connection__user = "${toString secrets.user.ghost-db}";
       database__connection__password = "${toString secrets.password.ghost-db}";
       database__connection__database = "ghost";
-      url = "http://${secrets.ip.ghost}:2368";
+      url = "${secrets.domain.main}";
     };
     extraOptions = [
       "--network=macvlan_lan"
