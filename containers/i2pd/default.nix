f3ced7435b101e388d599b5cc89bbfbf333d6f51
kye <kyesarri@gmail.com>
2024-10-28 23:15:17 +1100

    28.10.24 - tweak i2pd

--- containers/i2pd/default.nix
+++ containers/i2pd/default.nix
@@ -6,7 +6,7 @@
   contName = "i2pd";
   dir1 = "/etc/oci.cont/${contName}";
 in {
-  system.activationScripts."make${contName}Dir" = lib.stringAfter ["var"] ''mkdir -v -p ${toString dir1} && chown 1000 ${toString dir1}'';
+  system.activationScripts."make${contName}Dir" = lib.stringAfter ["var"] ''mkdir -v -p ${toString dir1} && chown 1000:1000 ${toString dir1}'';
 
   virtualisation.oci-containers.containers."${contName}" = {
     hostname = "${contName}";
@@ -20,7 +20,10 @@ in {
       "${dir1}:/home/i2pd/data/"
     ];
 
-    environment = {};
+    environment = {
+      PUID = "1000";
+      PGID = "1000";
+    };
 
     extraOptions = [
       "--network=macvlan_lan"
