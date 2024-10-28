44a203973c995b8b00ea3d8f018000854ce8ff7d
kye <kyesarri@gmail.com>
2024-10-28 23:49:37 +1100

    28.10.24 - tweak i2pd

--- containers/i2pd/default.nix
+++ containers/i2pd/default.nix
@@ -4,13 +4,11 @@
   ...
 }: let
   contName = "i2pd";
-  dir1 = "/etc/oci.cont/${contName}/data";
+  dir1 = "/etc/oci.cont/${contName}";
 in {
-  system.activationScripts."make${contName}Dir" =
-    lib.stringAfter ["var"]
-    ''
-      mkdir -v -p ${toString dir1} certificates destinations && chown -R 1000:1000 ${toString dir1}
-    '';
+  system.activationScripts."make${contName}Dir" = lib.stringAfter ["var"] ''mkdir -v -p ${toString dir1}'';
+
+  #  && chown -R 1000:1000 ${toString dir1}
 
   virtualisation.oci-containers.containers."${contName}" = {
     hostname = "${contName}";
@@ -24,10 +22,12 @@ in {
       "${dir1}:/home/i2pd/data/"
     ];
 
-    environment = {
+    /*
+      environment = {
       PUID = "1000";
       PGID = "1000";
     };
+    */
 
     extraOptions = [
       "--network=macvlan_lan"
