73e8e2befef6fb0fe4e39c8619f78836452a6bc1
kye <kyesarri@gmail.com>
2024-10-28 23:22:14 +1100

    28.10.24 - tweak i2pd

--- containers/i2pd/default.nix
+++ containers/i2pd/default.nix
@@ -4,9 +4,14 @@
   ...
 }: let
   contName = "i2pd";
-  dir1 = "/etc/oci.cont/${contName}";
+  dir1 = "/etc/oci.cont/${contName}/data";
 in {
-  system.activationScripts."make${contName}Dir" = lib.stringAfter ["var"] ''mkdir -v -p ${toString dir1} && chown 1000:1000 ${toString dir1}'';
+  system.activationScripts."make${contName}Dir" =
+    lib.stringAfter ["var"]
+    ''
+      mkdir -v -p ${toString dir1} &&
+      chown -R 1000:1000 ${toString dir1}
+    '';
 
   virtualisation.oci-containers.containers."${contName}" = {
     hostname = "${contName}";
