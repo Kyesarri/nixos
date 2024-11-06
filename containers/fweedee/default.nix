5becdce16100034dd6b42b4fb98f86e333da2c27
kye <kyesarri@gmail.com>
2024-11-06 21:51:27 +1100

    06.11.24 - fweedee change back to pods...

--- containers/fweedee/default.nix
+++ containers/fweedee/default.nix
@@ -62,7 +62,7 @@ in {
       ];
       script = ''
         ${pkgs.podman}/bin/podman pod exists ${prefix}-backend || \
-          ${pkgs.podman}/bin/podman pod create -n ${prefix}-backend -p '${secrets.ip.erying}:80:80'
+          ${pkgs.podman}/bin/podman pod create -n ${prefix}-backend
       '';
     };
     "create-${prefix}-frontend-pod" = with config.virtualisation.oci-containers; {
@@ -76,7 +76,7 @@ in {
       ];
       script = ''
         ${pkgs.podman}/bin/podman pod exists ${prefix}-frontend || \
-          ${pkgs.podman}/bin/podman pod create -n ${prefix}-frontend
+          ${pkgs.podman}/bin/podman pod create -n ${prefix} -p '${secrets.ip.erying}:80:80'
       '';
     };
   };
