8994aaed021718fca8865979d618b8f7a405946e
kye <kyesarri@gmail.com>
2024-11-07 00:22:14 +1100

    06.11.24 - fweedee test c2n

--- containers/fweedee/default.nix
+++ containers/fweedee/default.nix
@@ -1,184 +1,237 @@
-# Auto-generated using compose2nix v0.2.3.
+# inspo from https://github.com/mkuf/prind
+# using container images from prind xoxo
+# containers all start - now to configure everything ;)
 {
+  config,
   pkgs,
   lib,
   ...
-}: {
-  # Containers
-  virtualisation.oci-containers.containers."fweedee-init" = {
-    image = "busybox:latest";
-    volumes = [
-      "/home/kel/c2n:/prind:rw"
-    ];
-    cmd = ["chown" "-R" "1000:1000" "/prind/config"];
-    labels = {
-      "org.prind.service" = "init";
-    };
-    log-driver = "journald";
-    extraOptions = [
-      "--network-alias=init"
-      "--network=fweedee_default"
-    ];
+}: let
+  # config happens here, change values as you desire
+  dir = "/etc/oci.cont/fweedee"; # set our root directory for containers
+  time = "/etc/localtime:/etc/localtime:ro"; # read only our server time to containers
+  prefix = "fweedee";
+
+  shared = {
+    logs = "${toString dir}/shared/logs";
+    run = "${toString dir}/shared/run";
+    config = "${toString dir}/shared/config";
+    gcodes = "${toString dir}/shared/gcodes";
   };
-  systemd.services."podman-fweedee-init" = {
-    serviceConfig = {
-      Restart = lib.mkOverride 500 "no";
-    };
-    after = [
-      "podman-network-fweedee_default.service"
-    ];
-    requires = [
-      "podman-network-fweedee_default.service"
-    ];
-    partOf = [
-      "podman-compose-fweedee-root.target"
-    ];
-    wantedBy = [
-      "podman-compose-fweedee-root.target"
-    ];
-  };
-  virtualisation.oci-containers.containers."fweedee-klipper" = {
+
+  klipper = {
+    dir = "${toString dir}/klipper";
+    name = "${prefix}-klipper";
     image = "mkuf/klipper:latest";
-    volumes = [
-      "/dev:/dev:rw"
-      "/home/kel/c2n/config:/opt/printer_data/config:rw"
-      "fweedee_gcode:/opt/printer_data/gcodes:rw"
-      "fweedee_log:/opt/printer_data/logs:rw"
-      "fweedee_run:/opt/printer_data/run:rw"
-    ];
-    cmd = ["-I" "printer_data/run/klipper.tty" "-a" "printer_data/run/klipper.sock" "printer_data/config/printer.cfg" "-l" "printer_data/logs/klippy.log"];
-    labels = {
-      "org.prind.service" = "klipper";
-    };
-    dependsOn = [
-      "fweedee-init"
-    ];
-    log-driver = "none";
-    extraOptions = [
-      "--network-alias=klipper"
-      "--network=fweedee_default"
-      "--privileged"
-    ];
-  };
-  systemd.services."podman-fweedee-klipper" = {
-    serviceConfig = {
-      Restart = lib.mkOverride 500 "always";
-    };
-    after = [
-      "podman-network-fweedee_default.service"
-      "podman-volume-fweedee_gcode.service"
-      "podman-volume-fweedee_log.service"
-      "podman-volume-fweedee_run.service"
-    ];
-    requires = [
-      "podman-network-fweedee_default.service"
-      "podman-volume-fweedee_gcode.service"
-      "podman-volume-fweedee_log.service"
-      "podman-volume-fweedee_run.service"
-    ];
-    partOf = [
-      "podman-compose-fweedee-root.target"
-    ];
-    wantedBy = [
-      "podman-compose-fweedee-root.target"
-    ];
-  };
-  virtualisation.oci-containers.containers."fweedee-traefik" = {
-    image = "traefik:3.2";
-    volumes = [
-      "/var/run/docker.sock:/var/run/docker.sock:ro"
-    ];
-    ports = [
-      "80:80/tcp"
-    ];
-    cmd = ["--accesslog" "--providers.docker=true" "--providers.docker.exposedbydefault=false" "--entrypoints.web.address=:80"];
-    labels = {
-      "org.prind.service" = "traefik";
-    };
-    log-driver = "journald";
-    extraOptions = [
-      "--network-alias=traefik"
-      "--network=fweedee_default"
-    ];
-  };
-  systemd.services."podman-fweedee-traefik" = {
-    serviceConfig = {
-      Restart = lib.mkOverride 500 "always";
-    };
-    after = [
-      "podman-network-fweedee_default.service"
-    ];
-    requires = [
-      "podman-network-fweedee_default.service"
-    ];
-    partOf = [
-      "podman-compose-fweedee-root.target"
-    ];
-    wantedBy = [
-      "podman-compose-fweedee-root.target"
-    ];
   };
 
-  # Networks
-  systemd.services."podman-network-fweedee_default" = {
-    path = [pkgs.podman];
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-      ExecStop = "podman network rm -f fweedee_default";
-    };
-    script = ''
-      podman network inspect fweedee_default || podman network create fweedee_default
-    '';
-    partOf = ["podman-compose-fweedee-root.target"];
-    wantedBy = ["podman-compose-fweedee-root.target"];
+  moonraker = {
+    dir = "${toString dir}/moonraker";
+    name = "${prefix}-moonraker";
+    image = "mkuf/moonraker:latest";
+    db = "${moonraker.dir}";
   };
 
-  # Volumes
-  systemd.services."podman-volume-fweedee_gcode" = {
-    path = [pkgs.podman];
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-    };
-    script = ''
-      podman volume inspect fweedee_gcode || podman volume create fweedee_gcode
-    '';
-    partOf = ["podman-compose-fweedee-root.target"];
-    wantedBy = ["podman-compose-fweedee-root.target"];
-  };
-  systemd.services."podman-volume-fweedee_log" = {
-    path = [pkgs.podman];
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-    };
-    script = ''
-      podman volume inspect fweedee_log || podman volume create fweedee_log --opt=device=tmpfs --opt=type=tmpfs
-    '';
-    partOf = ["podman-compose-fweedee-root.target"];
-    wantedBy = ["podman-compose-fweedee-root.target"];
-  };
-  systemd.services."podman-volume-fweedee_run" = {
-    path = [pkgs.podman];
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-    };
-    script = ''
-      podman volume inspect fweedee_run || podman volume create fweedee_run --opt=device=tmpfs --opt=type=tmpfs
-    '';
-    partOf = ["podman-compose-fweedee-root.target"];
-    wantedBy = ["podman-compose-fweedee-root.target"];
+  octoprint = {
+    dir = "${toString dir}/octoprint";
+    name = "${prefix}-octoprint";
+    image = "octoprint/octoprint:latest";
   };
 
-  # Root service
-  # When started, this will automatically create all resources and start
-  # the containers. When stopped, this will teardown all resources.
-  systemd.targets."podman-compose-fweedee-root" = {
-    unitConfig = {
-      Description = "Root target generated by compose2nix.";
+  fluidd = {
+    name = "${prefix}-fluidd";
+    image = "ghcr.io/fluidd-core/fluidd:latest";
+  };
+  mainsail = {
+    name = "${prefix}-mainsail";
+    image = "ghcr.io/mainsail-crew/mainsail:edge";
+  };
+in {
+  #
+  # create a systemd service to bring up our pod
+  systemd.services = {
+    "create-${prefix}-backend-netwok" = with config.virtualisation.oci-containers; {
+      serviceConfig.Type = "oneshot";
+      wantedBy = [
+        "podman-${klipper.name}.service"
+        "podman-${moonraker.name}.service"
+        "podman-${octoprint.name}.service"
+        "podman-${fluidd.name}.service"
+        "podman-${mainsail.name}.service"
+      ];
+      script = ''
+        ${pkgs.podman}/bin/podman network exists ${prefix} || \
+          ${pkgs.podman}/bin/podman network create -d bridge ${prefix}
+      '';
     };
-    wantedBy = ["multi-user.target"];
+  };
+  /*
+  systemd.services."fweedee" = {
+    path = [pkgs.zfs pkgs.podman];
+
+    description = "Start podman '${prefix}' pod";
+
+    # don't start without network-online being up
+    wants = ["network-online.target"];
+    after = ["network-online.target"];
+
+    # our containers require fweedee pod to be up before they start
+    requiredBy = [
+      "podman-${klipper.name}.service"
+      "podman-${moonraker.name}.service"
+      "podman-${octoprint.name}.service"
+      "podman-${fluidd.name}.service"
+      "podman-${mainsail.name}.service"
+    ];
+
+    # unsure, might not be required?
+    unitConfig.RequiresMountsFor = "/run/containers";
+
+    # script to run (create the pod)
+
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.podman}/bin/podman pod create -n ${prefix} -p '127.0.0.1:80:80'";
+    };
+  };
+  */
+  # create container dirs
+  system.activationScripts = {
+    #
+    # probably not required but here for shits n' gigs
+    "make${dir}dir" = lib.stringAfter ["var"] ''mkdir -v -p ${dir}'';
+
+    # shared
+    "makeshareddir" = lib.stringAfter ["var"] ''mkdir -v -p ${shared.logs} ${shared.run} ${shared.config} ${shared.gcodes}'';
+
+    # moonraker
+    "make${moonraker.name}dir" = lib.stringAfter ["var"] ''mkdir -v -p ${moonraker.dir}'';
+
+    # octoprint
+    "make${octoprint.name}dir" = lib.stringAfter ["var"] ''mkdir -v -p ${octoprint.dir}'';
+
+    # chown dirs
+    "make${dir}owner" = lib.stringAfter ["var"] ''chown -R 1000:1000 ${toString dir} && echo pwnd'';
+  };
+  # configs, from tree to container dirs
+  environment.etc = {
+    "oci.cont/${prefix}/shared/config/printer.cfg" = {
+      mode = "644";
+      uid = 1000;
+      gid = 1000;
+      source = ./printer.cfg;
+    };
+
+    "oci.cont/${prefix}/shared/config/moonraker.conf" = {
+      mode = "644";
+      uid = 1000;
+      gid = 1000;
+      source = ./moonraker.conf;
+    };
+  };
+  # containers
+  virtualisation.oci-containers.containers = {
+    #
+    ${klipper.name} = {
+      hostname = "${klipper.name}";
+      autoStart = true;
+      image = "${klipper.image}";
+
+      volumes = [
+        "${time}"
+        "/dev:/dev"
+        "${toString shared.config}:/opt/printer_data/config"
+        "${toString shared.gcodes}:/opt/printer_data/gcodes"
+        "${toString shared.logs}:/opt/printer_data/logs"
+        "${toString shared.run}:/opt/printer_data/run"
+      ];
+
+      cmd = [
+        "-I"
+        "printer_data/run/klipper.tty"
+        "-a"
+        "printer_data/run/klipper.sock"
+        "printer_data/config/printer.cfg"
+        "-l"
+        "printer_data/logs/klippy.log"
+      ];
+
+      extraOptions = [
+        "--privileged"
+        "--network=fweedee"
+      ];
+    };
+    #
+    ${moonraker.name} = {
+      hostname = "${moonraker.name}";
+      autoStart = true;
+      image = "${moonraker.image}";
+
+      volumes = [
+        "${time}"
+        "/dev/null:/opt/klipper/config/null"
+        "/dev/null:/opt/klipper/docs/null"
+        "/run/dbus:/run/dbus"
+        "/run/systemd:/run/systemd"
+        "${moonraker.db}:/opt/printer_data/database"
+        "${shared.config}:/opt/printer_data/config"
+        "${shared.gcodes}:/opt/printer_data/gcodes"
+        "${shared.logs}:/opt/printer_data/logs"
+        "${shared.run}:/opt/printer_data/run"
+      ];
+
+      extraOptions = [
+        "--privileged"
+        "--network=fweedee"
+      ];
+    };
+    #
+    ${octoprint.name} = {
+      hostname = "${octoprint.name}";
+      autoStart = true;
+      image = "${octoprint.image}";
+
+      volumes = [
+        "${time}"
+        "/dev:/dev"
+        "${octoprint.dir}:/octoprint"
+        "${shared.run}:/opt/printer_data/run"
+      ];
+
+      extraOptions = [
+        "--privileged"
+        "--network=fweedee"
+      ];
+    };
+    #
+    ${fluidd.name} = {
+      hostname = "${fluidd.name}";
+      autoStart = true;
+      image = "${fluidd.image}";
+      ports = ["80:80"];
+      volumes = ["${time}"];
+      extraOptions = [
+        "--privileged"
+        "--network=fweedee"
+      ];
+    };
+    #
+    ${mainsail.name} = {
+      hostname = "${mainsail.name}";
+      autoStart = true;
+      image = "${mainsail.image}";
+      volumes = ["${time}"];
+      extraOptions = [
+        "--network=fweedee"
+      ];
+    };
   };
 }
+/*
+# wanting to test out this vs activationScripts
+systemd.tmpfiles.rules = [
+  "d ${toString dir} 0770 1000 1000 -" # not needed? since this is recursive?
+  "d -"
+];
+*/
+
