99e481d77db83af3a453210b9ed46ebf6c3a33f4
kye <kyesarri@gmail.com>
2024-11-06 16:24:48 +1100

    06.11.24 - fweedee change back to pods...

--- hosts/erying/networking.nix
+++ hosts/erying/networking.nix
@@ -16,7 +16,7 @@
     firewall = {
       enable = true;
       checkReversePath = "loose"; # fixes connection issues with tailscale
-      allowedTCPPorts = [22 123 80 3493];
+      allowedTCPPorts = [22 123 80 8080 3493];
       allowedUDPPorts = [config.services.tailscale.port 123];
     };
   };
