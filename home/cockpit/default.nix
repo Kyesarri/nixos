6ff8fef07adbe92586ca9c845d0f2785753d2d3f
kye <kyesarri@gmail.com>
2024-11-14 16:59:43 +1100

    14.11.24 - cockpit home testing - bad code keke

--- home/cockpit/default.nix
+++ home/cockpit/default.nix
@@ -1,6 +1,6 @@
 # https://fictionbecomesfact.com/nixos-cockpit-podman
 {pkgs, ...}: let
-  cockpit-apps = pkgs.callPackage ./cockpit-podman.nix;
+  cockpit-apps = pkgs.callPackage ./cockpit-podman.nix {inherit pkgs;};
 in {
   environment.systemPackages = with pkgs; [
     cockpit
