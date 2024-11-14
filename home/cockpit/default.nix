bddd1cddc3dea3286f7be317133f3165dedb0585
kye <kyesarri@gmail.com>
2024-11-14 17:06:14 +1100

    14.11.24 - cockpit home testing - bad code keke

--- home/cockpit/default.nix
+++ home/cockpit/default.nix
@@ -1,4 +1,2 @@
 # https://fictionbecomesfact.com/nixos-cockpit-podman
-{pkgs, ...}: {
-  podman-containers = pkgs.callPackage ./cockpit-podman.nix {};
-}
+{pkgs, ...}: {podman-containers = pkgs.callPackage ./podman-containers.nix {};}
