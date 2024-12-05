2f045848bf9773452fb650c32780d0faf5f5c3f3
kye <kyesarri@gmail.com>
2024-12-05 13:34:08 +1100

    05.12.24 - remove some comments from per-device hyprland

--- hosts/laptop/per-device.nix
+++ hosts/laptop/per-device.nix
@@ -3,10 +3,7 @@
     text = ''
       # monitor = name, resolution(@hz *optional*), position, scale
       monitor = eDP-1, 1920x1080@120, auto, 1
-      # monitor = HDMI-A-1, 3840x1600, 0x0, 1 # comment out - possibly causing issues with build from 02.09.24 NOPE!
-
-      # bindl =, switch:Lid Switch, exec, swaylock --screenshots --effect-blur 3x5 --fade-in 0.7 --effect-vignette 0.1:0.8
-      # binds lid switch to run swaylock script, swaylock smashing cpu on desktop, byeeeeeee
+      monitor = HDMI-A-1, 3840x1600, 0x0, 1
 
       # env = WLR_DRM_DEVICES,/dev/dri/card0:/dev/dri/card1
       # gpu preference for hypr, set to prefer integrated but will use dedicated if integrated isnt available
