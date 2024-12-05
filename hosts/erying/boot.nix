d52f2a0136fc708539c1d1a738b999cb8f0e43ff
kye <kyesarri@gmail.com>
2024-12-05 13:34:30 +1100

    05.12.24 - formatting

--- hosts/erying/boot.nix
+++ hosts/erying/boot.nix
@@ -10,6 +10,7 @@
       "net.ipv4.ip_forward" = "1"; # for tailscale exit node
       "net.ipv6.conf.all.forwarding" = "1"; # for tailscale exit node
     };
+
     supportedFilesystems = ["zfs"];
 
     initrd.systemd.enable = true;
