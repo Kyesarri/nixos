f7b58d49a87e7ef63e53e4ea78577de1099d6cdb
kye <kyesarri@gmail.com>
2025-01-13 18:13:09 +1100

    13.01.25 - add moonraker test configs

--- home/fwedee/default.nix
+++ home/fwedee/default.nix
@@ -1,4 +1,4 @@
-{...}: {
+{secrets, ...}: {
   # barebones - notebook to replace rasp pi 3a+ "soon"
   services = {
     mainsail = {
@@ -8,11 +8,20 @@
       enable = true;
       configFile = ./printer.cfg;
     };
+    /*
     fluidd = {
       enable = true;
     };
+    */
     moonraker = {
       enable = true;
+      address = "0.0.0.0";
+      allowSystemControl = true;
+      settings.authorization = {
+        force_logins = true;
+        trusted_clients = ["${secrets.ip.subnet}/24" "127.0.0.1/32"];
+        cors_domains = ["*.home"];
+      };
     };
   };
 }
