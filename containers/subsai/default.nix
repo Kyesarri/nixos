4da4d4e46e0578b8a6ff1009e5bf2e954921286d
kye <kyesarri@gmail.com>
2025-02-02 10:19:03 +1100

    02.02.25 - subsai comment puid / pgid

--- containers/subsai/default.nix
+++ containers/subsai/default.nix
@@ -92,8 +92,8 @@ in {
 
         environment = {
           TZ = "${cfg.timeZone}";
-          PUID = "1000";
-          PGID = "1000";
+          # PUID = "1000";
+          # PGID = "1000";
         };
 
         extraOptions = [
