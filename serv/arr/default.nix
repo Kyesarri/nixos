2704990d4dd3de02c72cf264ccd628235646b772
kye <kyesarri@gmail.com>
2025-02-05 22:04:02 +1100

    05.02.25 - arr - add incomplete dir, toggle incomplete dir

--- serv/arr/default.nix
+++ serv/arr/default.nix
@@ -43,7 +43,7 @@
         idle-seeding-limit-enabled = true;
         trash-original-torrent-files = true;
         incomplete-dir-enabled = true;
-        incomplete-dir = /hddg/incomplete;
+        incomplete-dir = "/hddg/incomplete";
         dht-enabled = true;
         download-dir = "/hddf/torrents/";
         download-queue-enabled = false;
