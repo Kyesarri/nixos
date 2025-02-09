ffdc8921ee23e5b3b08d577bb5342c0cdd015186
kye <kyesarri@gmail.com>
2025-02-09 21:10:30 +1100

    09.02.25 - add additional chrony server

--- home/chrony/default.nix
+++ home/chrony/default.nix
@@ -7,9 +7,8 @@
     directory = "/var/lib/chrony";
     servers = [
       "0.au.pool.ntp.org"
-      "1.au.pool.ntp.org"
-      "2.pool.ntp.org"
-      "3.ntp.per.nml.csiro.au"
+      "1.pool.ntp.org"
+      "2.ntp.per.nml.csiro.au"
     ];
     extraConfig = ''allow ${secrets.ip.subnet}/${secrets.ip.mask}'';
   };
