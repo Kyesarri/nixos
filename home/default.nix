86de30269840bf591ea1d3d0c94e17c57d814706
kye <kyesarri@gmail.com>
2025-03-12 13:02:23 +1100

    12.03.25 - merge console.nix /home/default.nix

--- home/default.nix
+++ home/default.nix
@@ -1,4 +1,8 @@
-{spaghetti, ...}: {
+{
+  spaghetti,
+  config,
+  ...
+}: {
   imports = [
     ./freetube
     ./gscreenshot
@@ -6,7 +10,30 @@
     ./nebula
     ./nemo
   ];
-
+  console = {
+    earlySetup = true;
+    useXkbConfig = true; # same config for linux console
+    colors = [
+      # terminal8
+      "${config.colorScheme.palette.base00}" # black
+      "${config.colorScheme.palette.base08}" # red
+      "${config.colorScheme.palette.base0B}" # green
+      "${config.colorScheme.palette.base0A}" # yellow
+      "${config.colorScheme.palette.base0D}" # blue
+      "${config.colorScheme.palette.base0E}" # magenta
+      "${config.colorScheme.palette.base0C}" # cyan
+      "${config.colorScheme.palette.base05}" # white
+      # terminal16
+      "${config.colorScheme.palette.base03}" # bright black
+      "${config.colorScheme.palette.base09}" # bright red
+      "${config.colorScheme.palette.base0B}" # bright green
+      "${config.colorScheme.palette.base0A}" # bright yellow
+      "${config.colorScheme.palette.base0D}" # bright blue
+      "${config.colorScheme.palette.base0E}" # bright magenta
+      "${config.colorScheme.palette.base0C}" # bright cyan
+      "${config.colorScheme.palette.base07}" # bright white
+    ];
+  };
   home-manager.users.${spaghetti.user} = {inputs, ...}: {
     colorscheme = {
       slug = "horizon-dark";
