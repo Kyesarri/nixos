2299a26190218741536ed7f87a90dfea765e557a
kye <kyesarri@gmail.com>
2025-03-12 13:07:25 +1100

    12.03.25 - move console config to module under ~/nixos/home/console

---
+++ home/console/default.nix
@@ -0,0 +1,27 @@
+{config, ...}: {
+  # set our console theme to match nix-colors
+  console = {
+    earlySetup = true;
+    useXkbConfig = true; # same config for linux console
+    colors = [
+      # terminal8
+      "${config.colorScheme.palette.base00}" # black
+      "${config.colorScheme.palette.base08}" # red
+      "${config.colorScheme.palette.base0B}" # green
+      "${config.colorScheme.palette.base0A}" # yellow
+      "${config.colorScheme.palette.base0D}" # blue
+      "${config.colorScheme.palette.base0E}" # magenta
+      "${config.colorScheme.palette.base0C}" # cyan
+      "${config.colorScheme.palette.base05}" # white
+      # terminal16
+      "${config.colorScheme.palette.base03}" # bright black
+      "${config.colorScheme.palette.base09}" # bright red
+      "${config.colorScheme.palette.base0B}" # bright green
+      "${config.colorScheme.palette.base0A}" # bright yellow
+      "${config.colorScheme.palette.base0D}" # bright blue
+      "${config.colorScheme.palette.base0E}" # bright magenta
+      "${config.colorScheme.palette.base0C}" # bright cyan
+      "${config.colorScheme.palette.base07}" # bright white
+    ];
+  };
+}
--- home/default.nix
+++ home/default.nix
@@ -1,9 +1,7 @@
-{
-  spaghetti,
-  config,
-  ...
-}: {
+{spaghetti, ...}: {
+  # import modules, most with options some are basic / required for all without options
   imports = [
+    ./console
     ./freetube
     ./gscreenshot
     ./hypr
@@ -11,34 +9,16 @@
     ./nemo
   ];
 
-  # set our console theme to match nix-colors
-  console = {
-    earlySetup = true;
-    useXkbConfig = true; # same config for linux console
-    colors = [
-      # terminal8
-      "${config.colorScheme.palette.base00}" # black
-      "${config.colorScheme.palette.base08}" # red
-      "${config.colorScheme.palette.base0B}" # green
-      "${config.colorScheme.palette.base0A}" # yellow
-      "${config.colorScheme.palette.base0D}" # blue
-      "${config.colorScheme.palette.base0E}" # magenta
-      "${config.colorScheme.palette.base0C}" # cyan
-      "${config.colorScheme.palette.base05}" # white
-      # terminal16
-      "${config.colorScheme.palette.base03}" # bright black
-      "${config.colorScheme.palette.base09}" # bright red
-      "${config.colorScheme.palette.base0B}" # bright green
-      "${config.colorScheme.palette.base0A}" # bright yellow
-      "${config.colorScheme.palette.base0D}" # bright blue
-      "${config.colorScheme.palette.base0E}" # bright magenta
-      "${config.colorScheme.palette.base0C}" # bright cyan
-      "${config.colorScheme.palette.base07}" # bright white
-    ];
-  };
-
   # home-manager config(s)
   home-manager.users.${spaghetti.user} = {inputs, ...}: {
+    # import flake home-manager modules
+    imports = [
+      inputs.nix-colors.homeManagerModules.default
+      inputs.prism.homeModules.prism
+      inputs.hyprland.homeManagerModules.default
+      inputs.schizofox.homeManagerModules.default
+    ];
+
     colorscheme = {
       slug = "horizon-dark";
       palette = {
@@ -60,14 +40,6 @@
         base0F = "E4A382";
       };
     };
-
-    # import flake home-manager modules
-    imports = [
-      inputs.nix-colors.homeManagerModules.default
-      inputs.prism.homeModules.prism
-      inputs.hyprland.homeManagerModules.default
-      inputs.schizofox.homeManagerModules.default
-    ];
 
     programs.home-manager.enable = true;
     xdg.enable = true;
