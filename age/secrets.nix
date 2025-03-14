5cef33fdea884e380763bb779fefda311bae6921
kye <kyesarri@gmail.com>
2025-03-14 12:01:06 +1100

    14.03.25 - testing agenix again

--- age/secrets.nix
+++ age/secrets.nix
@@ -1,10 +1,10 @@
 let
-  kel = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGEYPPXbG3EGxGlcfFveZKgTDn5Y64eTFfa7B27hObwK root@nix-laptop";
+  kel = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEFnfRc5Gx998u6eDONtNutO5McC2ggpuhePHG6Zr00p kel@nix-laptop";
   users = [kel];
 
-  system1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPJDyIr/FSz1cJdcoW69R+NrWzwGK/+3gJpqD1t8L2zE";
-  systems = [system1];
+  nix-laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGEYPPXbG3EGxGlcfFveZKgTDn5Y64eTFfa7B27hObwK root@nix-laptop";
+  systems = [nix-laptop];
 in {
-  "secret1.age".publicKeys = [kel system1];
+  "secret1.age".publicKeys = [kel nix-laptop];
   "secret2.age".publicKeys = users ++ systems;
 }
