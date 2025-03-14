4ed1b69a8e3723d8e2937699d13857fee24a9a17
kye <kyesarri@gmail.com>
2025-03-14 13:44:40 +1100

    14.03.25 - agenix basic lad config finished, will be used eventually

--- age/secrets.nix
+++ age/secrets.nix
@@ -1,13 +1,15 @@
 let
+  # users
   kel-laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEFnfRc5Gx998u6eDONtNutO5McC2ggpuhePHG6Zr00p kel@nix-laptop";
   kel-erying = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMON98uxNe7PXNbMMDiJbjqaz/qvNACfWsBW24KZGiWR kel@nix-erying";
   users = [kel-laptop kel-erying];
 
+  # systems
   nix-laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGEYPPXbG3EGxGlcfFveZKgTDn5Y64eTFfa7B27hObwK root@nix-laptop";
   nix-erying = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIcItUOpimL/3wF2qj27C8/58s0ChrC9xauvDgyX0SPC root@nixos";
   systems = [nix-laptop nix-erying];
   #
 in {
-  "secret1.age".publicKeys = [kel-erying kel-laptop nix-laptop nix-erying];
+  "secret1.age".publicKeys = users ++ systems;
   "secret2.age".publicKeys = users ++ systems;
 }
