7557cacbe4c8fcbcaac4e63a67a2380ec9dfdf00
kye <kyesarri@gmail.com>
2025-03-25 01:13:43 +1100

    25.03.25 - more formatting

--- home/waybar/default.nix
+++ home/waybar/default.nix
@@ -9,14 +9,11 @@
   ];
 
   home-manager.users.${spaghetti.user} = {
+    home.file.".config/hypr/per-app/waybar.conf".text = ''exec-once = waybar'';
+
     programs.waybar = {
       enable = true;
       package = pkgs.waybar.override (oldAttrs: {pulseSupport = true;});
-    };
-    home.file.".config/hypr/per-app/waybar.conf" = {
-      text = ''
-        exec-once = waybar
-      '';
     };
   };
 }
