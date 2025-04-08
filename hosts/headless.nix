7a1bc1f6bd7ba5e4a7605ad947591aa5f646fb0e
kye <kyesarri@gmail.com>
2025-04-08 13:04:30 +1000

    08.04.25 - add vbetool

--- hosts/headless.nix
+++ hosts/headless.nix
@@ -103,6 +103,7 @@
       ustreamer # smal usb camera streamer
       v4l-utils
       asciiquarium-transparent # for notebook, "screensaver"
+      vbetool # video bios tool - hoping to turn off "headless" systems
     ];
   };
 }
