a28f2a05cf9c7f1276e1f1c16b84b32d3d19a358
kye <kyesarri@gmail.com>
2025-04-14 12:39:20 +1000

    14.04.25 - more tests

--- home/spotifyd/default.nix
+++ home/spotifyd/default.nix
@@ -3,24 +3,28 @@
   pkgs,
   config,
   secrets,
+  spaghetti,
   ...
 }: {
-  services.spotifyd = {
-    enable = true;
-    settings.global = {
-      username = "${secrets.email.alternate}";
-      password_cmd = "${secrets.password.spotify}";
-      backend = "pulseaudio";
-      no_audio_cache = true;
-      bitrate = 320;
-      device_name = "spotify-${config.networking.hostName}";
-      device_type = "computer";
-      use_mpris = false;
+  home-manager.users.${spaghetti.user} = {pkgs, ...}: {
+    services.spotifyd = {
+      enable = true;
+      settings.global = {
+        username = "${secrets.email.alternate}";
+        password_cmd = "${secrets.password.spotify}";
+        backend = "pulseaudio";
+        no_audio_cache = true;
+        bitrate = 320;
+        device_name = "spotify-${config.networking.hostName}";
+        device_type = "computer";
+        use_mpris = false;
+      };
     };
+    home.packages = [pkgs.spotify-player];
   };
 
   # https://github.com/camillemndn/infra/tree/4c87eecbc7870b15d59d08c71092ab4aaea29040/profiles/spotifyd
-
+  /*
   systemd.services.spotifyd.serviceConfig = {
     DynamicUser = lib.mkForce false;
     SupplementaryGroups = lib.mkForce [
@@ -28,9 +32,7 @@
       "pipewire"
     ];
   };
-
-  environment.systemPackages = [pkgs.spotify-player pkgs.spotifyd];
-
+  */
   networking.firewall = {
     allowedTCPPorts = [5353];
     allowedUDPPorts = [5353];
