d0f5bb7901be2294b510e7558ee86c4d586845a3
kye <kyesarri@gmail.com>
2025-04-20 02:01:54 +1000

    20.04.25 - update to latest container configs

--- containers/i2pd/default.nix
+++ containers/i2pd/default.nix
@@ -1,31 +1,80 @@
 {
-  secrets,
+  config,
+  pkgs,
   lib,
   ...
-}: let
-  contName = "i2pd";
-
-  dir1 = "/etc/oci.cont/${contName}";
+}:
+with lib; let
+  cfg = config.cont.i2pd;
 in {
-  system.activationScripts."make${contName}Dir" = lib.stringAfter ["var"] ''mkdir -v -p ${dir1}'';
-
-  virtualisation.oci-containers.containers."${contName}" = {
-    hostname = "${contName}";
-
-    autoStart = true;
-
-    image = "purplei2p/i2pd:latest";
-
-    volumes = [
-      "/etc/localtime:/etc/localtime:ro"
-      "${dir1}/.i2pd:/home/.i2pd/"
-      "${dir1}/i2pd:/home/i2pd/"
-    ];
-
-    cmd = ["--http.address ${secrets.ip.i2pd}" "--port=${secrets.port.i2pd}"];
-
-    extraOptions = [
-      "--network=macvlan_lan:ip=${secrets.ip.i2pd}"
-    ];
+  options.cont.i2pd = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+    };
+    timeZone = mkOption {
+      type = types.str;
+      default = "Australia/Melbourne";
+    };
   };
+
+  config = mkMerge [
+    (mkIf (cfg.enable == true) {
+      systemd = {
+        targets."podman-i2pd-root" = {
+          unitConfig = {Description = "root target";};
+          wantedBy = ["multi-user.target"];
+        };
+        services = {
+          # container
+          "podman-i2pd" = {
+            serviceConfig = {Restart = lib.mkOverride 90 "always";};
+            after = [
+              "podman-network-internal.service"
+              "podman-volumes-i2pd.service"
+            ];
+            requires = [
+              "podman-network-internal.service"
+              "podman-volumes-i2pd.service"
+            ];
+            partOf = ["podman-i2pd-root.target"];
+            wantedBy = ["podman-i2pd-root.target"];
+          };
+          # volumes
+          "podman-volumes-i2pd" = {
+            path = [pkgs.podman];
+            serviceConfig = {
+              Type = "oneshot";
+              RemainAfterExit = true;
+            };
+            script = ''
+              podman volume inspect .i2pd || podman volume create .i2pd && \
+              podman volume inspect i2pd || podman volume create i2pd
+            '';
+            partOf = ["podman-i2pd-root.target"];
+            wantedBy = ["podman-i2pd-root.target"];
+          };
+        };
+
+        virtualisation.oci-containers.containers = {
+          "i2pd" = {
+            image = "purplei2p/i2pd:latest";
+            environment = {
+              TZ = "${cfg.timeZone}";
+            };
+            volumes = [
+              "/etc/localtime:/etc/localtime:ro"
+              ".i2pd:/home/.i2pd/"
+              "i2pd:/home/i2pd/"
+            ];
+            # cmd = ["--http.address ${secrets.ip.i2pd}" "--port=${secrets.port.i2pd}"];
+            extraOptions = [
+              "--network-alias=i2pd"
+              "--network=internal"
+            ];
+          };
+        };
+      };
+    })
+  ];
 }
