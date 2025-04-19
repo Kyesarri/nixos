4533719b687a3eb78ec7b22af3fbf8be6cb213d5
kye <kyesarri@gmail.com>
2025-04-20 01:25:37 +1000

    20.04.25 - update to internal podman network config

--- containers/jellyfin/default.nix
+++ containers/jellyfin/default.nix
@@ -10,78 +10,75 @@ with lib; let
   cfg = config.cont.jellyfin;
 in {
   options.cont.jellyfin = {
-    #
     enable = mkOption {
       type = types.bool;
       default = false;
-      example = true;
-      description = "enable container";
     };
-    #
-    vlanIp = mkOption {
-      type = types.nullOr types.str;
-      default = "";
-      example = "12.12.12.1";
-      description = "backend network ip address";
-    };
-    #
-    contName = mkOption {
-      type = types.str;
-      default = "jellyfin-${config.networking.hostName}";
-      example = "container-cool-hostname";
-      description = "container name, is also used for container volume dir name and activation script";
-    };
-
     timeZone = mkOption {
       type = types.str;
       default = "Australia/Melbourne";
-      example = "Australia/Broken_Hill";
-      description = "database timezone";
-    };
-    #
-    image = mkOption {
-      type = types.str;
-      default = "linuxserver/jellyfin:latest";
-      example = "jellyfin/jellyfin:latest";
-      description = "container image";
     };
   };
 
   config = mkMerge [
     (mkIf (cfg.enable == true) {
-      #
-      system.activationScripts."make${cfg.contName}dir" =
-        lib.stringAfter ["var"]
-        ''mkdir -v -p /etc/oci.cont/${cfg.contName}/config mkdir -v -p /etc/oci.cont/${cfg.contName}/data & chown -R 1000:1000 /etc/oci.cont/${cfg.contName}'';
-
-      environment.shellAliases = {cont-jellyfin = "sudo podman pull ${cfg.image}";};
-
-      virtualisation.oci-containers.containers.${cfg.contName} = {
-        hostname = "${cfg.contName}";
-
-        autoStart = true;
-
-        image = "${cfg.image}";
-
-        volumes = [
-          "/etc/localtime:/etc/localtime:ro"
-
-          "/dev/dri:/dev/dri" # hardware accel;
-
-          "/etc/oci.cont/${cfg.contName}/config:/config"
-          "/etc/oci.cont/${cfg.contName}/data:/core/data"
-        ];
-
-        environment = {
-          TZ = "${cfg.timeZone}";
-          PUID = "1000";
-          PGID = "1000";
+      systemd = {
+        targets."podman-jellyfin-root" = {
+          unitConfig = {Description = "root target";};
+          wantedBy = ["multi-user.target"];
         };
+        services = {
+          # container
+          "podman-jellyfin" = {
+            serviceConfig = {Restart = lib.mkOverride 90 "always";};
+            after = [
+              "podman-network-internal.service"
+              "podman-volumes-jellyfin.service"
+            ];
+            requires = [
+              "podman-network-internal.service"
+              "podman-volumes-jellyfin.service"
+            ];
+            partOf = ["podman-jellyfin-root.target"];
+            wantedBy = ["podman-jellyfin-root.target"];
+          };
+          # data volume
+          "podman-volumes-jellyfin" = {
+            path = [pkgs.podman];
+            serviceConfig = {
+              Type = "oneshot";
+              RemainAfterExit = true;
+            };
+            script = ''
+              podman volume inspect jellyfin-data || podman volume create jellyfin-data && \
+              podman volume inspect jellyfin-config || podman volume create jellyfin-config
+            '';
+            partOf = ["podman-jellyfin-root.target"];
+            wantedBy = ["podman-jellyfin-root.target"];
+          };
+        };
+      };
 
-        extraOptions = [
-          "--privileged"
-          "--network=podman-backend:ip=${cfg.vlanIp}"
-        ];
+      virtualisation.oci-containers.containers = {
+        "jellyfin" = {
+          image = "linuxserver/jellyfin:latest";
+          volumes = [
+            "/etc/localtime:/etc/localtime:ro"
+            "/dev/dri:/dev/dri" # hardware accel;
+            "jellyfin-config:/config"
+            "jellyfin-data:/core/data"
+          ];
+          environment = {
+            TZ = "${cfg.timeZone}";
+            PUID = "1000";
+            PGID = "1000";
+          };
+          extraOptions = [
+            "--privileged"
+            "--network-alias=jellyfin"
+            "--network=internal"
+          ];
+        };
       };
     })
   ];
