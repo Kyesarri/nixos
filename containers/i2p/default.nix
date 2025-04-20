e6997db31432b91173467a5a3913cadd6fe80022
kye <kyesarri@gmail.com>
2025-04-20 22:47:14 +1000

    20.04.25 - todo heaps

--- containers/i2p/default.nix
+++ containers/i2p/default.nix
@@ -1,7 +1,7 @@
 /*
 Enjoy a smooth and private browsing experience on multiple networks
 (like https://diva.exchange or also onion and i2p sites like http://diva.i2p or
-http://kopanyoc2lnsx5qwpslkik4uccej6zqna7qq2igbofhmb2qxwflwfqad.onion).
+http://kopanyoc2lnsx5qwpslkik4uccej6zqna7qq2igbofhmb2qxwflwfqad.onion)
 Use your favourite browser (like Firefox). Hence it should be suitable for beginners.
 
 Please note: an entry-level setup is only a first - yet necessary - step to protect your privacy.
