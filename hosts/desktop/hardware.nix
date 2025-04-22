6c8b6758abae7d1bd2a11a301204ed6f53a03579
kye <kyesarri@gmail.com>
2025-04-22 10:15:33 +1000

    22.04.25 - update device ids

--- hosts/desktop/hardware.nix
+++ hosts/desktop/hardware.nix
@@ -7,17 +7,17 @@
   imports = [(modulesPath + "/installer/scan/not-detected.nix")];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/d3b0895e-8994-4f75-b626-c8a261e2fdbb";
+    device = "/dev/disk/by-uuid/6fbe0d31-d02f-4c44-a0a2-f527653b130e";
     fsType = "ext4";
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/4693-85A6";
+    device = "/dev/disk/by-uuid/D596-74B1";
     fsType = "vfat";
+    options = ["fmask=0077" "dmask=0077"];
   };
 
-  swapDevices = [{device = "/dev/disk/by-uuid/d7f52e0c-cee6-4a8d-ad34-7d28de301208";}];
-
+  swapDevices = [{device = "/dev/disk/by-uuid/ade95298-b55d-4256-9ce5-e8cb93da19b8";}];
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
