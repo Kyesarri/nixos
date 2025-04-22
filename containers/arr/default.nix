ccc3cebfc8e12067a6c915e4dbd4f0c9b7e828cc
kye <kyesarri@gmail.com>
2025-04-23 09:12:56 +1000

    22.04.25 - add readarr, transmission, jellyfin and overseerr

--- containers/arr/default.nix
+++ containers/arr/default.nix
@@ -1,7 +1,9 @@
 /*
-arr - a stack of containers for managing downloads
-radarr, sonarr, bazarr and pyload-ng
-#TODO readarr
+arr - a stack of containers for managing downloads, requests and serving media
+radarr, sonarr, bazarr, overseerr, jellyfin, readarr, transmission#TODO, plex#TODO and pyload-ng
+
+no ports exposed on host system, nginx-wan / nginx-lan containers reverse proxy to containers
+on an internal podman network
 */
 {
   config,
@@ -151,7 +153,109 @@ in {
             partOf = ["podman-arr-root.target"];
             wantedBy = ["podman-arr-root.target"];
           };
-          # download dir shared between all containers - #TODO change where this is stored via script
+          # overseerr container
+          "podman-arr-overseerr" = {
+            serviceConfig = {Restart = lib.mkOverride 90 "always";};
+            after = [
+              "podman-network-arr.service"
+              "podman-volume-arr-overseerr.service"
+            ];
+            requires = [
+              "podman-network-arr.service"
+              "podman-volume-arr-overseerr.service"
+            ];
+            partOf = ["podman-arr-root.target"];
+            wantedBy = ["podman-arr-root.target"];
+          };
+          # overseerr volume
+          "podman-volume-overseerr" = {
+            path = [pkgs.podman];
+            serviceConfig = {
+              Type = "oneshot";
+              RemainAfterExit = true;
+            };
+            script = ''podman volume inspect arr-overseerr || podman volume create arr-overseerr'';
+            partOf = ["podman-arr-root.target"];
+            wantedBy = ["podman-arr-root.target"];
+          };
+          # readarr container
+          "podman-arr-readarr" = {
+            serviceConfig = {Restart = lib.mkOverride 90 "always";};
+            after = [
+              "podman-network-arr.service"
+              "podman-volume-arr-readarr.service"
+            ];
+            requires = [
+              "podman-network-arr.service"
+              "podman-volume-arr-readarr.service"
+            ];
+            partOf = ["podman-arr-root.target"];
+            wantedBy = ["podman-arr-root.target"];
+          };
+          # readarr volume
+          "podman-volume-readarr" = {
+            path = [pkgs.podman];
+            serviceConfig = {
+              Type = "oneshot";
+              RemainAfterExit = true;
+            };
+            script = ''podman volume inspect arr-readarr || podman volume create arr-readarr'';
+            partOf = ["podman-arr-root.target"];
+            wantedBy = ["podman-arr-root.target"];
+          };
+          # jellyfin container
+          "podman-jellyfin" = {
+            serviceConfig = {Restart = lib.mkOverride 90 "always";};
+            after = [
+              "podman-network-arr.service"
+              "podman-volume-jellyfin.service"
+            ];
+            requires = [
+              "podman-network-arr.service"
+              "podman-volume-arr-jellyfin.service"
+            ];
+            partOf = ["podman-arr-root.target"];
+            wantedBy = ["podman-arr-root.target"];
+          };
+          # jellyfin volume
+          "podman-volume-arr-jellyfin" = {
+            path = [pkgs.podman];
+            serviceConfig = {
+              Type = "oneshot";
+              RemainAfterExit = true;
+            };
+            script = ''podman volume inspect arr-jellyfin || podman volume create arr-jellyfin'';
+            partOf = ["podman-arr-root.target"];
+            wantedBy = ["podman-arr-root.target"];
+          };
+          # transmission container
+          "podman-transmission" = {
+            serviceConfig = {Restart = lib.mkOverride 90 "always";};
+            after = [
+              "podman-network-arr.service"
+              "podman-volume-transmission.service"
+              "podman-volume-arr-downloads.service"
+            ];
+            requires = [
+              "podman-network-arr.service"
+              "podman-volume-arr-transmission.service"
+              "podman-volume-arr-downloads.service"
+            ];
+            partOf = ["podman-arr-root.target"];
+            wantedBy = ["podman-arr-root.target"];
+          };
+          # transmission volume
+          "podman-volume-arr-transmission" = {
+            path = [pkgs.podman];
+            serviceConfig = {
+              Type = "oneshot";
+              RemainAfterExit = true;
+            };
+            script = ''podman volume inspect arr-transmission || podman volume create arr-transmission'';
+            partOf = ["podman-arr-root.target"];
+            wantedBy = ["podman-arr-root.target"];
+          };
+          # download dir shared between all containers - #TODO change where this is stored
           "podman-volume-arr-downloads" = {
             path = [pkgs.podman];
             serviceConfig = {
@@ -165,6 +269,7 @@ in {
         };
       };
 
+      # podman containers
       virtualisation.oci-containers.containers = {
         #
         "arr-radarr" = {
@@ -206,6 +311,7 @@ in {
             "--network=arr"
           ];
         };
+        #
         "arr-bazarr" = {
           image = "lscr.io/linuxserver/bazarr:latest";
           log-driver = "journald";
@@ -240,6 +346,84 @@ in {
           ];
           extraOptions = [
             "--network-alias=pyload"
+            "--network=arr"
+          ];
+        };
+        #
+        "arr-overseerr" = {
+          image = "lscr.io/linuxserver/overseerr:latest";
+          log-driver = "journald";
+          environment = {
+            TZ = "${cfg.timeZone}";
+            PUID = "1000";
+            PGID = "1000";
+          };
+          volumes = [
+            "/etc/localtime:/etc/localtime:ro"
+            "arr-overseerr:/app/config"
+          ];
+          extraOptions = [
+            "--network-alias=overseerr"
+            "--network=arr"
+          ];
+        };
+        #
+        "arr-readarr" = {
+          image = "lscr.io/linuxserver/readarr:latest";
+          log-driver = "journald";
+          environment = {
+            TZ = "${cfg.timeZone}";
+            PUID = "1000";
+            PGID = "1000";
+          };
+          volumes = [
+            "/etc/localtime:/etc/localtime:ro"
+            "arr-readarr:/config"
+            # "/path/to/books:/books" #TODO
+            "arr-downloads:/downloads"
+          ];
+          extraOptions = [
+            "--network-alias=readarr"
+            "--network=arr"
+          ];
+        };
+        #
+        "arr-transmission" = {
+          image = "lscr.io/linuxserver/transmission:latest";
+          log-driver = "journald";
+          environment = {
+            TZ = "${cfg.timeZone}";
+            PUID = "1000";
+            PGID = "1000";
+            DOCKER_MODS = "linuxserver/mods:transmission-floodui";
+          };
+          volumes = [
+            "/etc/localtime:/etc/localtime:ro"
+            "arr-transmission:/config"
+            "arr-downloads:/downloads"
+          ];
+          extraOptions = [
+            "--network-alias=transmission"
+            "--network=arr"
+          ];
+        };
+        #
+        "arr-jellyfin" = {
+          image = "lscr.io/linuxserver/jellyfin:latest";
+          log-driver = "journald";
+          environment = {
+            TZ = "${cfg.timeZone}";
+            PUID = "1000";
+            PGID = "1000";
+          };
+          volumes = [
+            "/etc/localtime:/etc/localtime:ro"
+            "arr-jellyfin:/config"
+            # "/path/to/tv_shows:/data/tvshows"
+            # "/path/to/movies:/data/movies"
+          ];
+          extraOptions = [
+            "--network-alias=jellyfin"
             "--network=arr"
           ];
         };
