ee3633244e4d0d3d2bffe7815ea61b67fa4bebb5
kye <kyesarri@gmail.com>
2025-04-25 18:37:14 +1000

    25.04.25 - update incomplete dir

--- containers/haos/default.nix
+++ containers/haos/default.nix
@@ -79,7 +79,6 @@ in {
         };
       };
       virtualisation.oci-containers.containers = {
-        # reverse proxy not working, even with x_forwarded_for true and nginx-lan added to trusted proxies :)
         "haos" = {
           image = "lscr.io/linuxserver/homeassistant:latest";
           log-driver = "journald";
