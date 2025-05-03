8d76e2c13989a10db6e6320c2cb235a571752deb
kye <kyesarri@gmail.com>
2025-05-04 09:40:50 +1000

    04.05.25 - add static address configs

--- home/syncthing/default.nix
+++ home/syncthing/default.nix
@@ -1,11 +1,12 @@
 {
-  pkgs,
   spaghetti,
+  secrets,
+  pkgs,
   ...
 }: {
+  # hyprland binds and window rules
   home-manager.users.${spaghetti.user} = {
     home.file.".config/hypr/per-app/syncthing.conf" = {
-      # hyprland binds and window rules
       text = ''
         # exec-once = sleep 10 && syncthing
         exec-once = sleep 10 && syncthingtray
@@ -15,17 +16,29 @@
     };
   };
 
+  # add tray icon
+  users.users.${spaghetti.user}.packages = with pkgs; [syncthingtray];
+
+  # syncthing service
   services = {
     syncthing = {
       enable = true;
       systemService = true;
+      # running system service under my user? idk
       user = "${spaghetti.user}";
-      configDir = "/home/${spaghetti.user}/.config/syncthing"; # folder for Syncthing's settings and keys
+      # folder for Syncthing's settings and keys
+      configDir = "/home/${spaghetti.user}/.config/syncthing";
+      # testing
+      devices = {
+        nix-erying = {
+          addresses = ["tcp://${secrets.ip.erying}:51820"];
+          id = "${secrets.syncthing.id.nix-erying}";
+        };
+        p7p = {
+          addresses = ["tcp://${secrets.ip.p7p}:51820"];
+          id = "${secrets.syncthing.id.p7p}";
+        };
+      };
     };
   };
-
-  # can also enable under ^^ as home-manager.users.${spaghetti.user}.services.syncthing.enable = true;
-
-  # add tray icon
-  users.users.${spaghetti.user}.packages = with pkgs; [syncthingtray];
 }
