e5926d0f45371de87024c69b0dd99434f94aaadb
kye <kyesarri@gmail.com>
2025-05-05 13:20:57 +1000

    05.05.25 - add additional hosts, change user, add port configs

--- home/syncthing/default.nix
+++ home/syncthing/default.nix
@@ -3,7 +3,10 @@
   secrets,
   pkgs,
   ...
-}: {
+}: let
+  statusPort = 22070;
+  relayPort = 22067;
+in {
   # hyprland binds and window rules
   home-manager.users.${spaghetti.user}.home.file.".config/hypr/per-app/syncthing.conf" = {
     text = ''
@@ -14,24 +17,42 @@
     '';
   };
 
-  # add tray icon
+  # add our user to the same group as syncthing user
+  users.groups.syncthing = {
+    name = "syncthing";
+    members = ["syncthing" "${spaghetti.user}"];
+  };
+
+  # open ports for syncthing
+  networking.firewall.allowedTCPPorts = [statusPort relayPort];
+
+  # add tray icon package
   users.users.${spaghetti.user}.packages = with pkgs; [syncthingtray];
 
-  # syncthing service
-  services = {
-    syncthing = {
-      enable = true;
-      systemService = true;
-      # running system service under my user? idk
-      user = "${spaghetti.user}";
-      # folder for Syncthing's settings and keys
-      configDir = "/home/${spaghetti.user}/.config/syncthing";
-      # testing
-      settings = {
-        devices = {
-          nix-erying = {id = "${secrets.syncthing.id.nix-erying}";};
-          p7p = {id = "${secrets.syncthing.id.p7p}";};
-        };
+  # don't create default ~/Sync folder
+  systemd.services.syncthing.environment.STNODEFAULTFOLDER = "true";
+
+  # syncthing service config
+  services.syncthing = {
+    enable = true;
+    systemService = true;
+    openDefaultPorts = true;
+    relay.statusPort = statusPort;
+    relay.port = relayPort;
+    user = "syncthing";
+    group = "syncthing";
+    /*
+    # TODO
+    key = "${</path/to/key.pem>}";
+    cert = "${</path/to/cert.pem>}";
+    # TODO
+    */
+    settings = {
+      devices = {
+        nix-erying = {id = "${secrets.syncthing.id.nix-erying}";};
+        p7p = {id = "${secrets.syncthing.id.p7p}";};
+        nix-laptop = {id = "${secrets.syncthing.id.nix-laptop}";};
+        nix-desktop = {id = "${secrets.syncthing.id.nix-desktop}";};
       };
     };
   };
