155a50b7c0761e0ca3a4073e0e09e2cfb088accc
kye <kyesarri@gmail.com>
2025-05-13 14:17:06 +1000

    13.05.25 - added lutris

--- home/gaming/default.nix
+++ home/gaming/default.nix
@@ -6,7 +6,9 @@
   home-manager.users.${spaghetti.user}.home.file.".config/hypr/per-app/gaming.conf" = {
     text = ''
       windowrule = fullscreen, title:Steam Big Picture Mode
-      windowrule = float, title:Steam
+      # windowrulev2 = size 1280 720, class:^(steam)$ causing mad dialouge popups
+      windowrulev2 = fullscreen, class:^(steam)$
+      windowrulev2 = bordercolor $cd, class:^(steam)$
     '';
   };
 
@@ -19,13 +21,15 @@
   services.ratbagd.enable = true; # mouse configuration tool
 
   users.users.${spaghetti.user}.packages = with pkgs; [
-    prismlauncher # minecraft launcher
+    # prismlauncher # minecraft launcher
     shattered-pixel-dungeon
     pcsx2 # ps2 emulator # TODO 22/01/24 has issues building, hash mismatch
     piper # frontend for libratbag
     protonup-qt # proton-ge # TODO get working with steam
     gamescope # wl roots gaming compositor, needs steam config not working currently
     ryujinx # nintendo switch emulator
+    lutris-unwrapped
+    wineWowPackages.waylandFull
     # ryujinx-greemdev # nintendo switch emulator fork #TODO 19.12.24 no worky - package not in store?
   ];
 
@@ -43,6 +47,7 @@
           stdenv.cc.cc.lib
           libkrb5
           keyutils
+          lutris-unwrapped
         ];
     };
   };
