a646c1d72b7ecf2608886b5adbffeb7b8f8c91a1
kye <kyesarri@gmail.com>
2025-05-18 22:02:17 +1000

    18.05.25 - uncomment username

--- home/ddclient/default.nix
+++ home/ddclient/default.nix
@@ -6,7 +6,7 @@
     usev4 = "webv4, webv4=ipv4.ident.me/";
     usev6 = "";
     domains = ["galing.org" "chat.galing.org" "request.galing.org" "ztnet.galing.org"];
-    # username = "${secrets.email.main}";
+    username = "${secrets.email.main}";
     passwordFile = "/root/cloudflare"; # manually added
     interval = "5min";
     verbose = true;
