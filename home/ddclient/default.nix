eb1825d960d03e994bfa7fa2dff273acae6d3f26
kye <kyesarri@gmail.com>
2025-05-18 10:25:56 +1000

    18.05.25 - tests :)

--- home/ddclient/default.nix
+++ home/ddclient/default.nix
@@ -4,7 +4,6 @@
     protocol = "cloudflare";
     zone = "galing.org";
     usev4 = "webv4, webv4=ipv4.ident.me/";
-    usev6 = "ifv6, ifv6=lan-self@eth0";
     domains = ["galing.org" "chat.galing.org" "request.galing.org" "ztnet.galing.org"];
     username = "${secrets.email.main}";
     passwordFile = "/root/cloudflare"; # manually added
