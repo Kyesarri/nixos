3787bac94de1d2e178232a52b577747116ad98d5
kye <kyesarri@gmail.com>
2025-05-18 15:31:58 +1000

    18.05.25 - rm www

--- home/ddclient/default.nix
+++ home/ddclient/default.nix
@@ -5,7 +5,7 @@
     zone = "galing.org";
     usev4 = "webv4, webv4=ipv4.ident.me/";
     usev6 = "";
-    domains = ["galing.org" "www.galing.org" "chat.galing.org" "request.galing.org" "ztnet.galing.org"];
+    domains = ["galing.org" "chat.galing.org" "request.galing.org" "ztnet.galing.org"];
     username = "${secrets.email.main}";
     passwordFile = "/root/cloudflare"; # manually added
     interval = "1min";
