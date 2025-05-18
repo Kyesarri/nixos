e4c19624138415c84154bbf222ba725c28d462e7
kye <kyesarri@gmail.com>
2025-05-18 10:12:35 +1000

    18.05.25 - add ipv6 config

--- home/ddclient/default.nix
+++ home/ddclient/default.nix
@@ -3,6 +3,7 @@
     enable = true;
     protocol = "cloudflare";
     usev4 = "webv4, webv4=ipv4.ident.me/";
+    usev6 = "ifv6, ifv6=eth0";
     zone = "galing.org";
     domains = ["galing.org" "chat.galing.org" "request.galing.org" "ztnet.galing.org"];
     username = "${secrets.email.main}";
