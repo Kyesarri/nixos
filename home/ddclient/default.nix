2bbf827645c1c7cda32497c0da8c4d54ba2712aa
kye <kyesarri@gmail.com>
2025-05-18 10:19:28 +1000

    18.05.25 - rm depreciated use

--- home/ddclient/default.nix
+++ home/ddclient/default.nix
@@ -2,9 +2,8 @@
   services.ddclient = {
     enable = true;
     protocol = "cloudflare";
-    use = "v4";
-    usev4 = "webv4, webv4=ipv4.ident.me/";
-    usev6 = "ifv6, ifv6=eth0";
+    usev4 = "ipv4";
+    usev6 = "";
     zone = "galing.org";
     domains = ["galing.org" "chat.galing.org" "request.galing.org" "ztnet.galing.org"];
     username = "${secrets.email.main}";
