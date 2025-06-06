d5b094a755ad130f3ef3e62c905bf86b072416b6
kye <kyesarri@gmail.com>
2025-06-06 10:52:59 +1000

    06.06.25 - rollback

--- containers/searx/default.nix
+++ containers/searx/default.nix
@@ -154,17 +154,19 @@
 
         # bit going on here, really just modifying some images / css / html
         # to change the theme on SearxNG page(s)
-        # want to take files from tree rather than leaving the absolute links here
+        # want to take files from tree rather than leaving the absolute paths here
+        # could add spaghetti but want to move away from that for container usage
 
         # images
-        # "/home/kel/nixos/containers/searx/favicon.svg:/usr/local/searxng/searx/static/themes/simple/img/favicon.svg:ro"
-        "./favicon.svg:/usr/local/searxng/searx/static/themes/simple/img/favicon.svg:ro"
-        "./searxng.svg:/usr/local/searxng/searx/static/themes/simple/img/searxng.svg:ro"
-
-        "/home/kel/nixos/containers/searx/page_with_header.html:/usr/local/searxng/searx/templates/simple/page_with_header.html:ro"
+        "/home/kel/nixos/containers/searx/favicon.svg:/usr/local/searxng/searx/static/themes/simple/img/favicon.svg:ro"
+        "/home/kel/nixos/containers/searx/searxng.svg:/usr/local/searxng/searx/static/themes/simple/img/searxng.svg:ro"
 
         "/home/kel/nixos/containers/searx/wordmark.min.svg:/usr/local/searxng/searx/templates/simple/wordmark.min.svg:ro"
         "/home/kel/nixos/containers/searx/wordmark.min.svg:/usr/local/searxng/searx/templates/simple/searxng-zwordmark.min.svg:ro"
+
+        # html
+        "/home/kel/nixos/containers/searx/page_with_header.html:/usr/local/searxng/searx/templates/simple/page_with_header.html:ro"
+
         # css
         "/home/kel/nixos/containers/searx/rss.min.css:/usr/local/searxng/searx/static/themes/simple/css/rss.min.css:ro"
         "/home/kel/nixos/containers/searx/searxng.min.css:/usr/local/searxng/searx/static/themes/simple/css/searxng.min.css:ro"
