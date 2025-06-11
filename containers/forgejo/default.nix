d34a9b73842d8296f7aa14b8760d8dc350e517df
kye <kyesarri@gmail.com>
2025-06-11 23:19:51 +1000

    11.06.25 - forgejo updates - testing custom dir

--- containers/forgejo/default.nix
+++ containers/forgejo/default.nix
@@ -21,14 +21,11 @@ in {
       type = types.bool;
       default = false;
     };
-    cloudflared-token = mkOption {
-      type = types.str;
-      default = "";
-    };
-    db-pass = mkOption {
-      type = types.str;
-      default = "";
-    };
+    cloudflared-token = mkOption {type = types.str;};
+    db-pass = mkOption {type = types.str;};
+    lfs_jwt_secret = mkOption {type = types.str;};
+    internal_token = mkOption {type = types.str;};
+    jwt_secret = mkOption {type = types.str;};
   };
 
   config = mkMerge [
@@ -36,7 +33,7 @@ in {
       # create dir on host for custom themes
       system.activationScripts."make-forgejo-theme-dir" =
         lib.stringAfter ["var"]
-        ''mkdir -v -p /etc/oci.cont/forgejo/css & chown -R 1000:1000 /etc/oci.cont/forgejo'';
+        ''mkdir -v -p /etc/oci.cont/forgejo/css /etc/oci.cont/forgejo/conf & chown -R 1000:1000 /etc/oci.cont/forgejo'';
 
       # symlink from tree to above dir on host
       environment.etc = {
@@ -46,6 +43,111 @@ in {
           uid = 1000;
           gid = 1000;
         };
+      };
+      "oci.cont/forgejo/custom/app.ini" = {
+        mode = "644";
+        uid = 1000;
+        gid = 1000;
+        text = ''
+          APP_NAME = galing
+          RUN_MODE = prod
+          RUN_USER = git
+          WORK_PATH = /data/gitea
+
+          [repository]
+          ROOT = /data/git/repositories
+
+          [repository.local]
+          LOCAL_COPY_PATH = /data/gitea/tmp/local-repo
+
+          [repository.upload]
+          TEMP_PATH = /data/gitea/uploads
+
+          [server]
+          APP_DATA_PATH = /data/gitea
+          DOMAIN = git.galing.org
+          SSH_DOMAIN = git.galing.org
+          HTTP_PORT = 3000
+          ROOT_URL = https://git.galing.org/
+          DISABLE_SSH = false
+          SSH_PORT = 22
+          SSH_LISTEN_PORT = 22
+          LFS_START_SERVER = true
+          LFS_JWT_SECRET = ${cfg.lfs_jwt_secret}
+          OFFLINE_MODE = true
+
+          [database]
+          PATH = /data/gitea/gitea.db
+          DB_TYPE = postgres
+          HOST = forgejo-db:5432
+          NAME = forgejo
+          USER = forgejo
+          PASSWD = ${cfg.db_pass}
+          LOG_SQL = false
+          SCHEMA = public
+          SSL_MODE = disable
+
+          [indexer]
+          ISSUE_INDEXER_PATH = /data/gitea/indexers/issues.bleve
+
+          [session]
+          PROVIDER_CONFIG = /data/gitea/sessions
+          PROVIDER = file
+
+          [picture]
+          AVATAR_UPLOAD_PATH = /data/gitea/avatars
+          REPOSITORY_AVATAR_UPLOAD_PATH = /data/gitea/repo-avatars
+
+          [attachment]
+          PATH = /data/gitea/attachments
+
+          [log]
+          MODE = console
+          LEVEL = info
+          ROOT_PATH = /data/gitea/log
+
+          [security]
+          INSTALL_LOCK = true
+          SECRET_KEY =
+          REVERSE_PROXY_LIMIT = 1
+          REVERSE_PROXY_TRUSTED_PROXIES = *
+          INTERNAL_TOKEN = ${cfg.internal_token}
+          PASSWORD_HASH_ALGO = pbkdf2_hi
+
+          [service]
+          DISABLE_REGISTRATION = true
+          REQUIRE_SIGNIN_VIEW = false
+          REGISTER_EMAIL_CONFIRM = false
+          ENABLE_NOTIFY_MAIL = false
+          ALLOW_ONLY_EXTERNAL_REGISTRATION = false
+          ENABLE_CAPTCHA = false
+          DEFAULT_KEEP_EMAIL_PRIVATE = false
+          DEFAULT_ALLOW_CREATE_ORGANIZATION = true
+          DEFAULT_ENABLE_TIMETRACKING = false
+          NO_REPLY_ADDRESS = noreply.localhost
+
+          [lfs]
+          PATH = /data/git/lfs
+
+          [mailer]
+          ENABLED = false
+
+          [openid]
+          ENABLE_OPENID_SIGNIN = true
+          ENABLE_OPENID_SIGNUP = true
+
+          [cron.update_checker]
+          ENABLED = true
+
+          [repository.pull-request]
+          DEFAULT_MERGE_STYLE = merge
+
+          [repository.signing]
+          DEFAULT_TRUST_MODEL = committer
+
+          [oauth2]
+          JWT_SECRET = ${cfg.jwt_secret}
+        '';
       };
 
       systemd = {
@@ -128,7 +230,7 @@ in {
             "FORGEJO__database__NAME" = "forgejo";
             "FORGEJO__database__PASSWD" = "${cfg.db-pass}";
             "FORGEJO__database__USER" = "forgejo";
-            "GITEA_CUSTOM" = "/data/gitea";
+            "FORGEJO_CUSTOM" = "/public/assets";
             "USER_GID" = "1000";
             "USER_UID" = "1000";
           };
