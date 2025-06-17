4033fff47fc527d92140fe230c9b754dfb6bb58a
kye <kyesarri@gmail.com>
2025-06-17 19:26:42 +1000

    17.06.25 - test more samba

--- hosts/erying/hardware.nix
+++ hosts/erying/hardware.nix
@@ -9,6 +9,16 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
+  environment.systemPackages = [pkgs.cifs-utils];
+  fileSystems."/mnt/hdda" = {
+    device = "//${secrets.ip.serv}/hdda";
+    fsType = "cifs";
+    options = let
+      # this line prevents hanging on network split
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,user,users";
+    in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=1000"];
+  };
+
   hardware = {
     cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
     enableRedistributableFirmware = lib.mkDefault true;
