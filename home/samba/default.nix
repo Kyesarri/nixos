c46ce6933d73344694f8c02b389152514f31fc01
kye <kyesarri@gmail.com>
2025-06-17 19:08:58 +1000

    17.06.25 - test more samba

--- home/samba/default.nix
+++ home/samba/default.nix
@@ -1,31 +1,42 @@
 /*
 https://nixos.wiki/wiki/Samba
 */
-{}: {
+{secrets, ...}: {
   services.samba = {
     enable = true;
+    securityType = "user";
     openFirewall = true;
     settings = {
       global = {
-        "guest account" = "samba-guest";
-        "map to guest" = "Bad User";
+        "workgroup" = "WORKGROUP";
+        "server string" = "smb-serv";
+        "netbios name" = "smb-serv";
+        "security" = "user";
+
+        "guest account" = "nobody";
+        "map to guest" = "bad user";
+
+        "hosts allow" = "192.168.87. 127.0.0.1 localhost";
+        "hosts deny" = "0.0.0.0/0";
       };
+
       "hdda" = {
         "path" = "/hdda";
         "browseable" = "yes";
         "read only" = "no";
         "writable" = "yes";
-        "force user" = "kel";
+        "force user" = "1000";
         "guest ok" = "no";
         "create mask" = "0640";
         "directory mask" = "0750";
       };
+
       "hddb" = {
-        "path" = "/hdda";
+        "path" = "/hddb";
         "browseable" = "yes";
         "read only" = "no";
         "writable" = "yes";
-        "force user" = "kel";
+        "force user" = "1000";
         "guest ok" = "no";
         "create mask" = "0640";
         "directory mask" = "0750";
