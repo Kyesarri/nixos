0d92680912f778a4a07981ac85269ba2019e8c60
kye <kyesarri@gmail.com>
2025-06-18 09:25:14 +1000

    18.06.25 - samba / mergefs brained my damage

--- hosts/erying/smb.nix
+++ hosts/erying/smb.nix
@@ -1,16 +1,32 @@
+/*
+this lad getting messy
+*/
 {
+  spaghetti,
   secrets,
   pkgs,
+  lib,
   ...
 }: {
   environment.systemPackages = [pkgs.cifs-utils];
+
+  services.samba.enable = true;
+
+  /*
+  security.wrappers."mount.cifs" = {
+    program = "mount.cifs";
+    source = "${lib.getBin pkgs.cifs-utils}/bin/mount.cifs";
+    owner = "root";
+    group = "root";
+    setuid = true;
+  };
+  */
 
   fileSystems = {
     "/mnt/storage" = {
       device = "//${secrets.ip.serv-1}/storage";
       fsType = "cifs";
       options = let
-        # this line prevents hanging on network split
         automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
       in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
     };
