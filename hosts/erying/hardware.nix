a927aa54d1bde53ec44d0fdfb4408d48455714fa
kye <kyesarri@gmail.com>
2025-06-17 19:32:23 +1000

    17.06.25 - typo

--- hosts/erying/hardware.nix
+++ hosts/erying/hardware.nix
@@ -13,7 +13,7 @@
   environment.systemPackages = [pkgs.cifs-utils];
   fileSystems."/mnt/hdda" = {
     device = "//${secrets.ip.serv-1}/hdda";
-    fsType = "cifs";
+    fsType = "auto";
     options = let
       # this line prevents hanging on network split
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,user,users";
