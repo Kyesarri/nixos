1fdbb0d2584c1b2f133322966db43f37903ac1bc
kye <kyesarri@gmail.com>
2025-06-17 19:17:55 +1000

    17.06.25 - test more samba

--- home/samba/default.nix
+++ home/samba/default.nix
@@ -1,5 +1,6 @@
 /*
 https://nixos.wiki/wiki/Samba
+evaluation warning: The option `services.samba.securityType' has been renamed to `services.samba.settings.global.security'.
 */
 {secrets, ...}: {
   services.samba = {
@@ -41,6 +42,31 @@ https://nixos.wiki/wiki/Samba
         "create mask" = "0640";
         "directory mask" = "0750";
       };
+    };
+  };
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      domain = true;
+      hinfo = true;
+      userServices = true;
+      workstation = true;
+    };
+    extraServiceFiles = {
+      smb = ''
+        <?xml version="1.0" standalone='no'?><!--*-nxml-*-->
+        <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+        <service-group>
+          <name replace-wildcards="yes">%h</name>
+          <service>
+            <type>_smb._tcp</type>
+            <port>445</port>
+          </service>
+        </service-group>
+      '';
     };
   };
 }
