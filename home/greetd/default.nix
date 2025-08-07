8d92d576d8b033ce00a6130708b4351734cb3c0c
kye <kyesarri@gmail.com>
2025-08-08 09:04:41 +1000

    08.08.25 - add comments

--- home/greetd/default.nix
+++ home/greetd/default.nix
@@ -6,26 +6,29 @@
   spaghetti,
   ...
 }: let
+  # used in greetd service
   tuigreet = "${pkgs.greetd.tuigreet}/bin/tuigreet";
   hyprland-session = "${inputs.hyprland.packages.${pkgs.system}.hyprland}/share/wayland-sessions";
   hyprland = "${inputs.hyprland.packages.${pkgs.system}.hyprland}/bin/Hyprland";
   username = "${spaghetti.user}";
 in {
-  environment.systemPackages = [pkgs.greetd.tuigreet];
-
   services.greetd = {
     enable = true;
     settings = {
+      # setup our autologin from boot
       initial_session = {
         command = "${hyprland}";
         user = "${username}";
       };
+      # default session after logging out, requires username / password
       default_session = {
         command = "${tuigreet} --time --remember --remember-user-session --sessions ${hyprland-session} --theme 'border=magenta;text=white;prompt=white;time=white;action=blue;button=yellow;container=darkgray;input=white'";
         user = "greeter";
       };
     };
   };
+
+  environment.systemPackages = [pkgs.greetd.tuigreet];
 
   systemd.services.greetd.serviceConfig = {
     Type = "idle";
