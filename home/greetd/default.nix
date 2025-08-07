c8d26f1c18de119cb2f606ce35a9fd4bdb3ae861
kye <kyesarri@gmail.com>
2025-08-08 09:00:32 +1000

    08.08.25 - add autologin on boot

--- home/greetd/default.nix
+++ home/greetd/default.nix
@@ -3,18 +3,25 @@
 {
   inputs,
   pkgs,
+  spaghetti,
   ...
 }: let
   tuigreet = "${pkgs.greetd.tuigreet}/bin/tuigreet";
   hyprland-session = "${inputs.hyprland.packages.${pkgs.system}.hyprland}/share/wayland-sessions";
+  hyprland = "${inputs.hyprland.packages.${pkgs.system}.hyprland}/bin/Hyprland";
+  username = "${spaghetti.user}";
 in {
   environment.systemPackages = [pkgs.greetd.tuigreet];
 
   services.greetd = {
     enable = true;
     settings = {
+      initial_session = {
+        command = "${hyprland}";
+        user = "${username}";
+      };
       default_session = {
-        command = "${tuigreet} --time --remember --remember-session --sessions ${hyprland-session} --theme 'border=magenta;text=white;prompt=white;time=white;action=blue;button=yellow;container=darkgray;input=white'";
+        command = "${tuigreet} --time --remember --remember-user-session --sessions ${hyprland-session} --theme 'border=magenta;text=white;prompt=white;time=white;action=blue;button=yellow;container=darkgray;input=white'";
         user = "greeter";
       };
     };
